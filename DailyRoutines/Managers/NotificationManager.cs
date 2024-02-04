3f91b185deafb80cc98017ba4fe26ebbc4ab6b5b
AtmoOmen <atmoomen2004@outlook.com>
2024-02-04 13:11:47 +0800

    NotificationManager

---
+++ DailyRoutines/Managers/NotificationManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyRoutines.Managers;
+
+public class NotificationManager
+{
+    [DllImport("winmm.dll", SetLastError = true)]
+    private static extern bool PlaySound(string pszSound, IntPtr hMod, uint fdwSound);
+
+    private static NotifyIcon? Icon;
+
+    private const uint SND_ASYNC = 0x0001;
+    public const uint SND_ALIAS = 0x00010000;
+
+    public static void ShowWindowsToast(string title, string content, ToolTipIcon icon = ToolTipIcon.Info)
+    {
+        if (Icon is not { Visible: true }) CreateIcon();
+
+        Task.Delay(3000).ContinueWith(_ => DestroyIcon());
+
+        PlaySound("SystemAsterisk", IntPtr.Zero, SND_ASYNC | SND_ALIAS);
+        Icon.ShowBalloonTip(int.MaxValue, string.IsNullOrEmpty(title) ? P.Name : title, content, icon);
+    }
+
+    private static void CreateIcon()
+    {
+        DestroyIcon();
+        Icon = new NotifyIcon
+        {
+            Icon = new Icon(Path.Join(P.PluginInterface.AssemblyLocation.DirectoryName, "Assets", "FFXIVICON.ico")),
+            Text = P.Name,
+            Visible = true
+        };
+    }
+
+    private static void DestroyIcon()
+    {
+        if (Icon != null)
+        {
+            Icon.Visible = false;
+            Icon.Dispose();
+            Icon = null;
+        }
+    }
+
+    internal static void Dispose()
+    {
+        if (Icon != null) Icon.Visible = false;
+        Icon?.Dispose();
+        Icon = null;
+    }
+}
