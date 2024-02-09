1a49402a50878188be97d25f55dd92d0c11c6c0c
AtmoOmen <atmoomen2004@outlook.com>
2024-02-09 11:09:26 +0800

    Update NotificationManager.cs

--- DailyRoutines/Managers/NotificationManager.cs
+++ DailyRoutines/Managers/NotificationManager.cs
@@ -1,8 +1,8 @@
+using ECommons.Automation;
 using System;
 using System.Drawing;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DailyRoutines.Managers;
@@ -13,16 +13,27 @@ public class NotificationManager
     private static extern bool PlaySound(string pszSound, IntPtr hMod, uint fdwSound);
 
     private static NotifyIcon? Icon;
+    private static TaskManager? TaskManager;
 
     private const uint SND_ASYNC = 0x0001;
     public const uint SND_ALIAS = 0x00010000;
+
+    internal void Init()
+    {
+        TaskManager ??= new TaskManager { AbortOnTimeout = false, TimeLimitMS = int.MaxValue, ShowDebug = false };
+    }
 
     public void ShowWindowsToast(string title, string content, ToolTipIcon icon = ToolTipIcon.Info)
     {
         if (Icon is not { Visible: true }) CreateIcon();
 
-        Task.Delay(2000).ContinueWith(_ => DestroyIcon());
+        TaskManager.Enqueue(() => ShowBalloonTip(title, content, icon));
+        TaskManager.DelayNext(2000);
+        TaskManager.Enqueue(DestroyIcon);
+    }
 
+    private void ShowBalloonTip(string title, string content, ToolTipIcon icon = ToolTipIcon.Info)
+    {
         PlaySound("SystemAsterisk", IntPtr.Zero, SND_ASYNC | SND_ALIAS);
         Icon.ShowBalloonTip(500, string.IsNullOrEmpty(title) ? P.Name : title, content, icon);
     }
@@ -40,6 +51,7 @@ public class NotificationManager
 
     private void DestroyIcon()
     {
+        if (TaskManager.NumQueuedTasks > 1) return;
         if (Icon != null)
         {
             Icon.Visible = false;
@@ -50,6 +62,8 @@ public class NotificationManager
 
     internal void Dispose()
     {
+        TaskManager?.Abort();
+
         if (Icon != null) Icon.Visible = false;
         Icon?.Dispose();
         Icon = null;
