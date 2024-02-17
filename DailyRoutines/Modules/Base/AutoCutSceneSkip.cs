30fdc4d393a7e06828173221ee1a6177ff69bb29
AtmoOmen <atmoomen2004@outlook.com>
2024-02-17 22:05:25 +0800

    精简

--- DailyRoutines/Modules/Base/AutoCutSceneSkip.cs
+++ DailyRoutines/Modules/Base/AutoCutSceneSkip.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using ClickLib;
 using DailyRoutines.Infos;
 using DailyRoutines.Managers;
@@ -20,13 +18,6 @@ public class AutoCutSceneSkip : IDailyModule
 {
     public bool Initialized { get; set; }
     public bool WithConfigUI => true;
-
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
-
-    private const uint Wm_Keydown = 0x0100;
-    private const uint Wm_Keyup = 0x0101;
-    private const int Vk_Esc = 0x1B;
 
     public void Init()
     {
@@ -49,17 +40,19 @@ public class AutoCutSceneSkip : IDailyModule
             {
                 if (Service.KeyState[Service.Config.ConflictKey])
                 {
-                    P.PluginInterface.UiBuilder.AddNotification(Service.Lang.GetText("ConflictKey-InterruptMessage"), "Daily Routines", NotificationType.Success);
+                    P.PluginInterface.UiBuilder.AddNotification(Service.Lang.GetText("ConflictKey-InterruptMessage"),
+                                                                "Daily Routines", NotificationType.Success);
                     return;
                 }
+
                 Task.Delay(10)
-                    .ContinueWith(_ => Service.AddonLifecycle.RegisterListener(AddonEvent.PostDraw, "NowLoading", OnAddonLoading));
+                    .ContinueWith(
+                        _ => Service.AddonLifecycle.RegisterListener(AddonEvent.PostDraw, "NowLoading",
+                                                                     OnAddonLoading));
                 Service.Toast.ErrorToast += OnErrorToast;
             }
             else
-            {
                 AbortActions();
-            }
         }
     }
 
@@ -68,30 +61,18 @@ public class AutoCutSceneSkip : IDailyModule
         if (message.ExtractText().Contains("该过场剧情无法跳过"))
         {
             AbortActions();
-            message = SeString.Empty; 
+            message = SeString.Empty;
             isHandled = true;
         }
     }
 
-    private static void OnAddonLoading(AddonEvent type, AddonArgs args)
+    private static unsafe void OnAddonLoading(AddonEvent type, AddonArgs args)
     {
-        PressEsc();
-        ClickExit();
-    }
-
-    private static void PressEsc()
-    {
-        var windowHandle = Process.GetCurrentProcess().MainWindowHandle;
-        PostMessage(windowHandle, Wm_Keydown, Vk_Esc, 0);
-        Task.Delay(50).ContinueWith(_ => PostMessage(windowHandle, Wm_Keyup, Vk_Esc, 0));
-    }
-
-    private static unsafe void ClickExit()
-    {
+        WindowsKeypress.SendKeypress(Keys.Escape);
         if (TryGetAddonByName<AtkUnitBase>("SystemMenu", out var menu) && IsAddonReady(menu))
         {
             Callback.Fire(menu, true, -1);
-            menu->IsVisible = false;
+            menu->Hide(true);
             AbortActions();
             return;
         }
@@ -99,9 +80,8 @@ public class AutoCutSceneSkip : IDailyModule
         if (TryGetAddonByName<AtkUnitBase>("SelectString", out var addon) && IsAddonReady(addon))
         {
             if (addon->GetTextNodeById(2)->NodeText.ExtractText().Contains("要跳过这段过场动画吗"))
-            {
-                if (Click.TrySendClick("select_string1")) AbortActions();
-            }
+                if (Click.TrySendClick("select_string1"))
+                    AbortActions();
         }
     }
 
