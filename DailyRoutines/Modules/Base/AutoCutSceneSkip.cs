2d6b9798579969fa427c844bf418316faa18d8a9
AtmoOmen <atmoomen2004@outlook.com>
2024-03-08 21:00:00 +0800

    fix 自动跳过过场剧情 reload error

--- DailyRoutines/Modules/Base/AutoCutSceneSkip.cs
+++ DailyRoutines/Modules/Base/AutoCutSceneSkip.cs
@@ -1,9 +1,11 @@
 using ClickLib;
 using DailyRoutines.Infos;
 using DailyRoutines.Managers;
+using Dalamud;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
-using ECommons.Automation;
+using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace DailyRoutines.Modules;
@@ -14,10 +16,20 @@ public class AutoCutSceneSkip : IDailyModule
     public bool Initialized { get; set; }
     public bool WithConfigUI => false;
 
+    private delegate void CutsceneHandleInputDelegate(nint a1);
+
+    [Signature("40 53 48 83 EC 20 80 79 29 00 48 8B D9 0F 85", DetourName = nameof(CutsceneHandleInputDetour))]
+    private static Hook<CutsceneHandleInputDelegate>? CutsceneHandleInputHook;
+
+    private const string ConditionSig = "75 11 BA ?? ?? ?? ?? 48 8B CF E8 ?? ?? ?? ?? 84 C0 74 52";
+    private static int ConditionOriginalValuesLen => ConditionSig.Split(" ").Length;
+    private static nint ConditionAddress;
+
     public void Init()
     {
-        AutoCutsceneSkipper.Init(null);
-        AutoCutsceneSkipper.Enable();
+        Service.Hook.InitializeFromAttributes(this);
+        ConditionAddress = Service.SigScanner.ScanText(ConditionSig);
+        CutsceneHandleInputHook?.Enable();
 
         Service.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "SelectString", OnAddon);
     }
@@ -35,9 +47,24 @@ public class AutoCutSceneSkip : IDailyModule
         }
     }
 
+    private static unsafe void CutsceneHandleInputDetour(nint a1)
+    {
+        var allowSkip = *(nint*)(a1 + 56) != 0;
+        if (allowSkip)
+        {
+            SafeMemory.WriteBytes(ConditionAddress, [0xEB]);
+            CutsceneHandleInputHook.Original(a1);
+            SafeMemory.WriteBytes(ConditionAddress, [0x75]);
+
+            return;
+        }
+
+        CutsceneHandleInputHook.Original(a1);
+    }
+
     public void Uninit()
     {
         Service.AddonLifecycle.UnregisterListener(OnAddon);
-        AutoCutsceneSkipper.Disable();
+        CutsceneHandleInputHook?.Dispose();
     }
 }
