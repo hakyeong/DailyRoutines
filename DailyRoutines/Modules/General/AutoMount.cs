6ec010e3799abb8fdc654e72b90e5c30e3dfc9b4
AtmoOmen <atmoomen2004@outlook.com>
2024-03-13 17:25:47 +0800

    优化逻辑

--- DailyRoutines/Modules/General/AutoMount.cs
+++ DailyRoutines/Modules/General/AutoMount.cs
@@ -19,7 +19,7 @@ public class AutoMount : IDailyModule
 
     public void Init()
     {
-        TaskManager ??= new TaskManager { AbortOnTimeout = true, TimeLimitMS = 20000, ShowDebug = false };
+        TaskManager ??= new TaskManager { AbortOnTimeout = true, TimeLimitMS = 20000, ShowDebug = true };
 
         Service.Condition.ConditionChange += OnConditionChanged;
         Service.ClientState.TerritoryChanged += OnZoneChanged;
@@ -33,6 +33,8 @@ public class AutoMount : IDailyModule
     private static void OnZoneChanged(ushort zone)
     {
         if (Service.PresetData.Contents.ContainsKey(zone)) return;
+
+        TaskManager.Abort();
         TaskManager.Enqueue(UseMountWhenZoneChanged);
     }
 
@@ -40,6 +42,7 @@ public class AutoMount : IDailyModule
     {
         if (flag is ConditionFlag.Gathering && !value)
         {
+            TaskManager.Abort();
             TaskManager.Enqueue(UseMountAfterGathering);
         }
     }
@@ -47,7 +50,6 @@ public class AutoMount : IDailyModule
     private static void OnErrorToast(ref SeString message, ref bool isHandled)
     {
         if (!TaskManager.IsBusy) return;
-
         var content = message.ExtractText();
 
         if (content.Contains("无法指定目标"))
@@ -81,12 +83,13 @@ public class AutoMount : IDailyModule
         if (Service.Condition[ConditionFlag.BetweenAreas]) return false;
         var addon = (AtkUnitBase*)Service.Gui.GetAddonByName("NowLoading");
         if (addon->IsVisible) return false;
+        var addon2 = (AtkUnitBase*)Service.Gui.GetAddonByName("FadeMiddle");
+        if (addon2->IsVisible) return false;
+        if (ActionManager.Instance()->GetActionStatus(ActionType.GeneralAction, 9) != 0) return true;
         if (AgentMap.Instance()->IsPlayerMoving == 1) return true;
         if (Service.Condition[ConditionFlag.Casting] | Service.Condition[ConditionFlag.Casting87]) return true;
         if (Service.Condition[ConditionFlag.Mounted] || Service.Condition[ConditionFlag.Mounted2]) return true;
 
-        TaskManager.Enqueue(() => ActionManager.Instance()->UseAction(ActionType.GeneralAction, 9));
-        TaskManager.DelayNext(100);
         TaskManager.Enqueue(() => ActionManager.Instance()->UseAction(ActionType.GeneralAction, 9));
 
         return true;
