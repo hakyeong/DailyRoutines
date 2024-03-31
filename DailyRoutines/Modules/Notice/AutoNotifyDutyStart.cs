7cff332d66963d3f7e5d8cd29827d7f5144a4804
AtmoOmen <atmoomen2004@outlook.com>
2024-03-31 20:37:23 +0800

    自动通知副本开始

---
+++ DailyRoutines/Modules/Notice/AutoNotifyDutyStart.cs
@@ -0,0 +1,31 @@
+using DailyRoutines.Infos;
+using DailyRoutines.Managers;
+using ImGuiNET;
+
+namespace DailyRoutines.Modules;
+
+[ModuleDescription("AutoNotifyDutyStartTitle", "AutoNotifyDutyStartDescription", ModuleCategories.Notice)]
+public class AutoNotifyDutyStart : DailyModuleBase
+{
+    private static bool ConfigOnlyNotifyWhenBackground;
+
+    public override void Init()
+    {
+        AddConfig(this, "OnlyNotifyWhenBackground", true);
+        ConfigOnlyNotifyWhenBackground = GetConfig<bool>(this, "OnlyNotifyWhenBackground");
+
+        Service.DutyState.DutyStarted += OnDutyStart;
+    }
+
+    public override void ConfigUI()
+    {
+        if (ImGui.Checkbox(Service.Lang.GetText("OnlyNotifyWhenBackground"), ref ConfigOnlyNotifyWhenBackground))
+            UpdateConfig(this, "OnlyNotifyWhenBackground", ConfigOnlyNotifyWhenBackground);
+    }
+
+    private static void OnDutyStart(object? sender, ushort e)
+    {
+        if (!ConfigOnlyNotifyWhenBackground || (ConfigOnlyNotifyWhenBackground && !HelpersOm.IsGameForeground()))
+            Service.Notice.Notify("", Service.Lang.GetText("AutoNotifyDutyStart-NotificationMessage"));
+    }
+}
