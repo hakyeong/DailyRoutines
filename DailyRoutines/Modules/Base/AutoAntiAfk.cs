aa85ea82e53227d061e0280b8f27d5dcb2d9c0dd
AtmoOmen <atmoomen2004@outlook.com>
2024-03-31 14:16:38 +0800

    重构 AutoAntiAfk

--- DailyRoutines/Modules/Base/AutoAntiAfk.cs
+++ DailyRoutines/Modules/Base/AutoAntiAfk.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Runtime.InteropServices;
 using System.Timers;
-using System.Windows.Forms;
 using DailyRoutines.Infos;
-using DailyRoutines.Managers;
-using Dalamud.Game.ClientState.Keys;
-using ECommons.Automation;
-using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using Timer = System.Timers.Timer;
 
 namespace DailyRoutines.Modules;
@@ -14,17 +8,6 @@ namespace DailyRoutines.Modules;
 [ModuleDescription("AutoAntiAfkTitle", "AutoAntiAfkDescription", ModuleCategories.Base)]
 public class AutoAntiAfk : DailyModuleBase
 {
-    [StructLayout(LayoutKind.Sequential)]
-    private struct LastInputInfo
-    {
-        public uint Size;
-        public uint LastInputTickCount;
-    }
-
-    [DllImport("User32.dll")]
-    private static extern bool GetLastInputInfo(ref LastInputInfo info);
-
-
     private static Timer? AfkTimer;
 
     public override void Init()
@@ -37,27 +20,14 @@ public class AutoAntiAfk : DailyModuleBase
 
     private static unsafe void OnAfkStateCheck(object? sender, ElapsedEventArgs e)
     {
-        var idleTime = GetIdleTime();
-        if (idleTime > TimeSpan.FromSeconds(10) || Framework.Instance()->WindowInactive)
+        var inputTimerModule = InputTimerModule.Instance();
+        if (inputTimerModule != null)
         {
-            switch (Service.Config.ConflictKey)
-            {
-                case VirtualKey.LCONTROL or VirtualKey.RCONTROL or VirtualKey.CONTROL:
-                    WindowsKeypress.SendKeypress(Keys.LShiftKey);
-                    break;
-                default:
-                    WindowsKeypress.SendKeypress(Keys.LControlKey);
-                    break;
-            }
+            inputTimerModule->AfkTimer = 0;
+            inputTimerModule->ContentInputTimer = 0;
+            inputTimerModule->InputTimer = 0;
+            inputTimerModule->Unk1C = 0;
         }
-    }
-
-    public static TimeSpan GetIdleTime()
-    {
-        var lastInputInfo = new LastInputInfo { Size = (uint)Marshal.SizeOf(typeof(LastInputInfo)) };
-        GetLastInputInfo(ref lastInputInfo);
-
-        return TimeSpan.FromMilliseconds(Environment.TickCount - (int)lastInputInfo.LastInputTickCount);
     }
 
     public override void Uninit()
