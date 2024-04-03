bfb305fdfd95f4409ff62c43b2e3a8420723d763
AtmoOmen <atmoomen2004@outlook.com>
2024-04-03 16:47:46 +0800

    Switch to gitmirror

--- DailyRoutines/Modules/Notice/AutoNotifyCutSceneEnd.cs
+++ DailyRoutines/Modules/Notice/AutoNotifyCutSceneEnd.cs
@@ -35,7 +35,7 @@ public class AutoNotifyCutSceneEnd : DailyModuleBase
     public override void ConfigUI()
     {
         PreviewImageWithHelpText(Service.Lang.GetText("AutoNotifyCutSceneEnd-NotificationMessageHelp"),
-                                 "https://raw.githubusercontent.com/AtmoOmen/DailyRoutines/main/imgs/AutoNotifyCutSceneEnd-1.png",
+                                 "https://raw.gitmirror.com/AtmoOmen/DailyRoutines/main/imgs/AutoNotifyCutSceneEnd-1.png",
                                  new Vector2(378, 113));
 
         if (ImGui.Checkbox(Service.Lang.GetText("OnlyNotifyWhenBackground"),
