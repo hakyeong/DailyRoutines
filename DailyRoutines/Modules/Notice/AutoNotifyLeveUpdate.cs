9a8a17a6a0298b37864353c406f2396856efa86f
LeiSangSang <1415823899@qq.com>
2024-04-09 16:53:19 +0800

    重写理符提醒

--- DailyRoutines/Modules/Notice/AutoNotifyLeveUpdate.cs
+++ DailyRoutines/Modules/Notice/AutoNotifyLeveUpdate.cs
@@ -4,27 +4,27 @@ using DailyRoutines.Infos;
 using DailyRoutines.Managers;
 using Dalamud.Plugin.Services;
 using ECommons.Throttlers;
+using FFXIVClientStructs.FFXIV.Client.Game;
 using ImGuiNET;
 
 namespace DailyRoutines.Modules;
 
 [ModuleDescription("AutoNotifyLeveUpdateTitle", "AutoNotifyLeveUpdateDescription", ModuleCategories.Notice)]
-public class AutoNotifyLeveUpdate : DailyModuleBase
+public unsafe class AutoNotifyLeveUpdate : DailyModuleBase
 {
     public override string? Author { get; set; } = "HSS";
-    private const string LeveSigScanner = "88 05 ?? ?? ?? ?? 0F B7 41 06";
     private static DateTime nextLeveCheck = DateTime.MinValue;
-    private static DateTime finishTime = DateTime.Now;
-    private static int lastLeve;
+    private static DateTime finishTime = DateTime.UtcNow;
+    private static int lastLeve = 0;
     private static bool OnChatMessage;
-    private static bool JustFull;
+    private static int NotificationThreshold;
 
     public override void Init()
     {
         AddConfig(this, "OnChatMessage", true);
-        AddConfig(this, "JustFull", true);
+        AddConfig(this, "NotificationThreshold", 97);
         OnChatMessage = GetConfig<bool>(this, "OnChatMessage");
-        JustFull = GetConfig<bool>(this, "JustFull");
+        NotificationThreshold = GetConfig<int>(this, "NotificationThreshold");
         Service.Framework.Update += OnFrameworkLeve;
     }
 
@@ -34,115 +34,82 @@ public class AutoNotifyLeveUpdate : DailyModuleBase
                                  "https://mirror.ghproxy.com/https://raw.githubusercontent.com/AtmoOmen/DailyRoutines/main/imgs/AutoNotifyLeveUpdate-1.png");
 
         ImGui.Spacing();
-
-        ImGui.Text(lastLeve == 100
-                       ? Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationFullText")
-                       : $"{Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationText")}{finishTime.ToString(CultureInfo.CurrentCulture)}");
-
-        ImGui.Text($"{Service.Lang.GetText("AutoNotifyLeveUpdate-LeveUpdateTimeText")}{nextLeveCheck}");
+        ImGui.Text($"{Service.Lang.GetText("AutoNotifyLeveUpdate-NumText")}{lastLeve}");
+        ImGui.Text($"{Service.Lang.GetText("AutoNotifyLeveUpdate-FullTimeText")}{finishTime.ToLocalTime().ToString(CultureInfo.CurrentCulture)}");
+        ImGui.Text($"{Service.Lang.GetText("AutoNotifyLeveUpdate-UpdateTimeText")}{nextLeveCheck.ToLocalTime().ToString(CultureInfo.CurrentCulture)}");
 
         if (ImGui.Checkbox(Service.Lang.GetText("AutoNotifyLeveUpdate-OnChatMessageConfig"),
                            ref OnChatMessage))
             UpdateConfig(this, "OnChatMessage", OnChatMessage);
-        if (ImGui.Checkbox(Service.Lang.GetText("AutoNotifyLeveUpdate-JustFullConfig"),
-                           ref JustFull))
-            UpdateConfig(this, "JustFull", JustFull);
+
+        if (ImGui.SliderInt(Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationThreshold"), ref NotificationThreshold, 1, 100))
+        {
+            lastLeve = 0;
+            UpdateConfig(this, "NotificationThreshold", NotificationThreshold);
+        }
+
+
     }
 
     private static void OnFrameworkLeve(IFramework _)
     {
         if (!EzThrottler.Throttle("AutoNotifyLeveUpdate", 5000)) return;
-        if (Service.ClientState.LocalPlayer == null)
+        if (Service.ClientState.LocalPlayer == null || !Service.ClientState.IsLoggedIn)
             return;
-        var firstFlag = nextLeveCheck == DateTime.MinValue;
-        var tempLastLeve = lastLeve;
-        var now = DateTime.Now;
-        var leves = Leves(Service.SigScanner.GetStaticAddressFromSig(LeveSigScanner));
-        leves = leves < 0 ? 0 : leves > 100 ? 100 : leves;
-        lastLeve = leves;
-        if (leves == tempLastLeve && tempLastLeve != 100) return;
-        var needDay = DaysToReachHundred(leves, now.TimeOfDay);
-        finishTime = now.AddDays(needDay);
-        if (finishTime.TimeOfDay >= TimeSpan.FromHours(20))
-            finishTime = new DateTime(finishTime.Year, finishTime.Month, finishTime.Day, 20, 0, 0);
-        else if (finishTime.TimeOfDay >= TimeSpan.FromHours(8))
-            finishTime = new DateTime(finishTime.Year, finishTime.Month, finishTime.Day, 8, 0, 0);
-        else
-            finishTime = new DateTime(finishTime.Year, finishTime.Month, finishTime.Day - 1, 20, 0, 0);
-        if (tempLastLeve <= leves)
+        var NowUtc = DateTime.UtcNow;
+        var leveAllowances = QuestManager.Instance()->NumLeveAllowances;
+        if (!lastLeve.Equals(leveAllowances))
         {
-            if (leves >= 97)
+            var decreasing = leveAllowances > lastLeve;
+            lastLeve = QuestManager.Instance()->NumLeveAllowances;
+            nextLeveCheck = MathNextTime(NowUtc);
+            finishTime = MathFinishTime(leveAllowances, NowUtc);
+
+            if (leveAllowances >= NotificationThreshold && decreasing)
             {
-                //理符要溢出或满的时候,只在刚上线和刷新五分钟前提醒
-                if (nextLeveCheck <= now.AddMinutes(5) || firstFlag)
+                if (OnChatMessage)
                 {
-                    if (OnChatMessage)
-                    {
-                        Service.Chat.Print($"{Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationTitle")}{leves}" +
-                                           $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationFullText")}" +
-                                           $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-LeveUpdateTimeText")}{nextLeveCheck.ToString(CultureInfo.CurrentCulture)}");
-                    }
-
-                    finishTime = now;
-                    Service.Notice.Notify($"{Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationTitle")}{leves}",
-                                          Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationFullText"));
-                    return;
+                    Service.Chat.Print($"{Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationTitle")}" +
+                                       $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-NumText")}{leveAllowances}" +
+                                       $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-FullTimeText")}{finishTime.ToLocalTime().ToString(CultureInfo.CurrentCulture)}" +
+                                       $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-UpdateTimeText")}{nextLeveCheck.ToLocalTime().ToString(CultureInfo.CurrentCulture)}");
                 }
-            }
 
-            if (JustFull) return;
-            if (OnChatMessage)
-            {
-                Service.Chat.Print(
-                    $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationText")}{finishTime.ToString(CultureInfo.CurrentCulture)}" +
-                    $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-LeveUpdateTimeText")}{nextLeveCheck.ToString(CultureInfo.CurrentCulture)}");
+                Service.Notice.Notify($"{Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationTitle")}",
+                                      $"{Service.Lang.GetText("AutoNotifyLeveUpdate-NumText")}{leveAllowances}" +
+                                      $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-FullTimeText")}{finishTime.ToLocalTime().ToString(CultureInfo.CurrentCulture)}" +
+                                      $"\n {Service.Lang.GetText("AutoNotifyLeveUpdate-UpdateTimeText")}{nextLeveCheck.ToLocalTime().ToString(CultureInfo.CurrentCulture)}");
             }
-
-            if (firstFlag) return;
-            Service.Notice.Notify($"{Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationTitle")}{leves}",
-                                  $"{Service.Lang.GetText("AutoNotifyLeveUpdate-NotificationText")}\n{finishTime.ToString(CultureInfo.CurrentCulture)}");
         }
     }
 
-    private static unsafe int Leves(IntPtr address)
+    private static DateTime MathNextTime(DateTime NowUtc)
     {
-        if (address != IntPtr.Zero)
-            return *(byte*)address;
-
-        return -1;
-    }
-
-    private static int DaysToReachHundred(int currentCount, TimeSpan currentTime)
-    {
-        var now = DateTime.Now;
-        const int dailyIncrement = 6;
-
-        var incrementToday = 0;
-
-        if (currentTime >= TimeSpan.FromHours(20))
+        if (NowUtc.Hour >= 12)
         {
-            nextLeveCheck = new DateTime(now.Year, now.Month, now.Day + 1, 8, 0, 0);
-            incrementToday += 6;
-        }
-        else if (currentTime >= TimeSpan.FromHours(8))
-        {
-            nextLeveCheck = new DateTime(now.Year, now.Month, now.Day, 20, 0, 0);
-            incrementToday += 3;
+            return new DateTime(NowUtc.Year, NowUtc.Month, NowUtc.Day + 1, 0, 0, 0, DateTimeKind.Utc);
         }
         else
         {
-            nextLeveCheck = new DateTime(now.Year, now.Month, now.Day, 8, 0, 0);
+            return new DateTime(NowUtc.Year, NowUtc.Month, NowUtc.Day, 12, 0, 0, DateTimeKind.Utc);
         }
+    }
 
 
-        var remaining = 100 - currentCount - incrementToday;
-
-        if (remaining <= 0)
-            return 0;
-
-        var daysNeeded = (int)Math.Ceiling((double)remaining / dailyIncrement);
-
-        return daysNeeded;
+    private static DateTime MathFinishTime(int num, DateTime NowUtc)
+    {
+        if (num >= 100)
+        {
+            return NowUtc;
+        }
+        var requiredIncrements = 100 - num;
+        var requiredPeriods = requiredIncrements / 3;
+        if (requiredIncrements % 3 > 0)
+        {
+            requiredPeriods++;
+        }
+        var lastIncrementTimeUtc = new DateTime(NowUtc.Year, NowUtc.Month, NowUtc.Day, NowUtc.Hour >= 12 ? 12 : 0, 0, 0, DateTimeKind.Utc);
+        return lastIncrementTimeUtc.AddHours(12 * requiredPeriods);
     }
 
     public override void Uninit()
