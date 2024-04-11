f5a9267af9311a2942f58fbde51716fefcfec3ef
AtmoOmen <atmoomen2004@outlook.com>
2024-04-11 19:33:46 +0800

    Update Widgets.cs

--- DailyRoutines/Infos/Widgets.cs
+++ DailyRoutines/Infos/Widgets.cs
@@ -1,17 +1,30 @@
 using System;
 using System.Numerics;
 using DailyRoutines.Managers;
+using DailyRoutines.Modules;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Game.Network.Structures.InfoProxy;
 using Dalamud.Interface.Colors;
 using Dalamud.Interface;
 using ECommons.ImGuiMethods;
 using ImGuiNET;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using Lumina.Excel.GeneratedSheets;
 using static DailyRoutines.Modules.QuickChatPanel;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Game.Text;
 
 namespace DailyRoutines.Infos;
 
 public static class Widgets
 {
+    public static SeString RPrefix(string text)
+    {
+        return new SeStringBuilder()
+        .AddUiForeground(SeIconChar.BoxedLetterR.ToIconString(), 34)
+        .AddUiForegroundOff().Append(text).Build();
+    } 
+
     public static void PreviewImageWithHelpText(
         string helpText, string imageUrl, Vector2 imageSize = default, FontAwesomeIcon imageIcon = FontAwesomeIcon.InfoCircle)
     {
@@ -55,5 +68,28 @@ public static class Widgets
         };
 
         return savedMacro;
+    }
+
+    public static unsafe ExpandPlayerMenuSearch.CharacterSearchInfo ToCharacterSearchInfo(this Character chara)
+    {
+        var info = new ExpandPlayerMenuSearch.CharacterSearchInfo()
+        {
+            Name = chara.Name.ExtractText(),
+            World = Service.Data.GetExcelSheet<World>()
+                           .GetRow(
+                               ((FFXIVClientStructs.FFXIV.Client.Game.Character.Character*)chara.Address)->HomeWorld)
+                           .Name.RawString
+        };
+        return info;
+    }
+
+    public static ExpandPlayerMenuSearch.CharacterSearchInfo ToCharacterSearchInfo(this CharacterData chara)
+    {
+        var info = new ExpandPlayerMenuSearch.CharacterSearchInfo()
+        {
+            Name = chara.Name,
+            World = chara.HomeWorld.GameData.Name.RawString
+        };
+        return info;
     }
 }
