76a5c9f7532ccb3b66f467d676aedbb8ce4cbbfc
AtmoOmen <atmoomen2004@outlook.com>
2024-04-17 15:48:18 +0800

    CommandManager 完善

--- DailyRoutines/Managers/CommandManager.cs
+++ DailyRoutines/Managers/CommandManager.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 using DailyRoutines.Windows;
 using Dalamud.Game.Command;
 
@@ -8,19 +8,40 @@ namespace DailyRoutines.Managers;
 public static class CommandManager
 {
     public const string CommandPDR = "/pdr";
-    private static readonly HashSet<string> addedCommands = [];
+    private static readonly Dictionary<string, CommandInfo> AddedCommands = [];
+    private static readonly Dictionary<string, CommandInfo> SubPDRArgs = [];
 
     public static void Init()
     {
-        AddCommand(CommandPDR, new CommandInfo(OnCommandPDR) { HelpMessage = Service.Lang.GetText("CommandHelp") }, true);
+        AddSubCommand("search", new CommandInfo(OnSubSearch) { HelpMessage = Service.Lang.GetText("CommandHelp-Search"), ShowInHelp = true });
+        RefreshCommandDetails();
     }
 
+    private static void RefreshCommandDetails()
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(Service.Lang.GetText("CommandHelp"));
+        foreach (var (command, commandInfo) in SubPDRArgs)
+        {
+            if (!commandInfo.ShowInHelp) continue;
+
+            stringBuilder.Append($"\n{CommandPDR} {command} → {commandInfo.HelpMessage}");
+        }
+
+        RemoveCommand(CommandPDR);
+        AddCommand(CommandPDR, new CommandInfo(OnCommandPDR) { HelpMessage = stringBuilder.ToString() }, true);
+    }
+
+    /// <summary>
+    /// 添加一个独立于 /pdr 的命令
+    /// </summary>
+    /// <param name="command">要添加的命令, 需要附带斜杠</param>
+    /// <param name="commandInfo"></param>
+    /// <param name="isForceToAdd">如果已有同名命令, 是否强制覆盖添加</param>
+    /// <returns></returns>
     public static bool AddCommand(string command, CommandInfo commandInfo, bool isForceToAdd = false)
     {
-        var addedCommand = Service.Command.Commands;
-        var isDuplicate = addedCommand.Keys.Any(x => x == command);
-
-        switch (isDuplicate)
+        switch (Service.Command.Commands.ContainsKey(command))
         {
             case true when !isForceToAdd:
                 return false;
@@ -29,16 +50,63 @@ public static class CommandManager
                 break;
         }
 
-        addedCommands.Add(command);
         Service.Command.AddHandler(command, commandInfo);
+        AddedCommands.Add(command, commandInfo);
+
         return true;
     }
 
+    /// <summary>
+    /// 移除已添加的命令
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns></returns>
     public static bool RemoveCommand(string command)
     {
-        if (Service.Command.Commands.Keys.All(x => x != command))
+        if (Service.Command.Commands.ContainsKey(command))
         {
             Service.Command.RemoveHandler(command);
+
+            AddedCommands.Remove(command);
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// 添加一个依附于 /pdr 的子命令
+    /// </summary>
+    /// <param name="args">子命令触发参数, 不需要斜杠</param>
+    /// <param name="commandInfo">你可以控制 ShowInHelp 属性来控制是否在详情页显示命令</param>
+    /// <param name="isForceToAdd">如果已有同名命令, 是否强制覆盖添加</param>
+    /// <returns></returns>
+    public static bool AddSubCommand(string args, CommandInfo commandInfo, bool isForceToAdd = false)
+    {
+        switch (SubPDRArgs.ContainsKey(args))
+        {
+            case true when !isForceToAdd:
+                return false;
+            case true when isForceToAdd:
+                SubPDRArgs.Remove(args);
+                break;
+        }
+
+        SubPDRArgs.Add(args, commandInfo);
+        RefreshCommandDetails();
+        return true;
+    }
+
+    /// <summary>
+    /// 删除一个依附于 /pdr 的子命令
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static bool RemoveSubCommand(string args)
+    {
+        if (SubPDRArgs.Remove(args))
+        {
+            RefreshCommandDetails();
             return true;
         }
 
@@ -47,23 +115,32 @@ public static class CommandManager
 
     private static void OnCommandPDR(string command, string args)
     {
-        if (!string.IsNullOrEmpty(args) && args != Main.SearchString)
+        if (string.IsNullOrWhiteSpace(args))
         {
-            Main.SearchString = args;
-            P.Main.IsOpen = true;
-        }
-        else
-        {
-            if (string.IsNullOrEmpty(args))
-                Main.SearchString = string.Empty;
-
+            Main.SearchString = string.Empty;
             P.Main.IsOpen ^= true;
         }
+
+        var spiltedArgs = args.Split(' ', 2);
+        if (SubPDRArgs.TryGetValue(spiltedArgs[0], out var commandInfo))
+            commandInfo.Handler(spiltedArgs[0], spiltedArgs.Length > 1 ? spiltedArgs[1] : "");
+    }
+
+    private static void OnSubSearch(string command, string args)
+    {
+        if (string.IsNullOrWhiteSpace(args))
+        {
+            OnCommandPDR("", "");
+            return;
+        }
+        Main.SearchString = args;
+        P.Main.IsOpen ^= true;
     }
 
     public static void Uninit()
     {
-        foreach (var command in addedCommands)
+        foreach (var command in AddedCommands.Keys)
             RemoveCommand(command);
+        SubPDRArgs.Clear();
     }
 }
