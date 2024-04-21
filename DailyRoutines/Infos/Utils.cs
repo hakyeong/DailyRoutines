900d9eaebfc401d0ed2c62f405fbf135c4443b39
LeiSangSang <1415823899@qq.com>
2024-04-21 15:06:02 +0800

    vnavmesh自动跟随

---
+++ DailyRoutines/Infos/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using DailyRoutines.Managers;
+using ECommons.Reflection;
+
+namespace DailyRoutines.Infos;
+
+public static unsafe class Utils
+{
+    public static bool HasPlugin(string name)
+    {
+        return Service.PluginInterface.InstalledPlugins.FirstOrDefault(x => x.InternalName == name) != null;
+    }
+
+    public static bool PluginLoadState(string name)
+    {
+        return Service.PluginInterface.InstalledPlugins.FirstOrDefault(x => x.InternalName == name)?.IsLoaded ?? false;
+    }
+
+    public static bool HasAndEnablePlugin(string name)
+    {
+        return HasPlugin(name) && PluginLoadState(name);
+    }
+}
