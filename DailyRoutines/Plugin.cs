3046bebbbbb4d116c477f6acff2b5b189d1a92b2
AtmoOmen <atmoomen2004@outlook.com>
2024-04-23 20:28:05 +0800

    Update Plugin.cs

--- DailyRoutines/Plugin.cs
+++ DailyRoutines/Plugin.cs
@@ -13,14 +13,23 @@ public sealed class Plugin : IDalamudPlugin
 {
     public static string Name => "Daily Routines";
 
+    private static bool IsDev;
+
     public Plugin(DalamudPluginInterface pluginInterface)
     {
+        // if (pluginInterface.IsDev)
+        // {
+        //     IsDev = true;
+        //     return;
+        // }
+
         ECommonsMain.Init(pluginInterface, this, Module.DalamudReflector);
         Service.Init(pluginInterface);
     }
 
     public void Dispose()
     {
+        if (IsDev) return;
         ECommonsMain.Dispose();
         Service.Uninit();
     }
