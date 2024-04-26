70d28ba3de5f7fd69116a326eb5675719c4532fd
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2024-04-26 23:35:06 +0800

    Create ExtraLuminaSheets.cs

---
+++ DailyRoutines/Infos/ExtraLuminaSheets.cs
@@ -0,0 +1,21 @@
+using Lumina.Excel;
+using Lumina;
+using Lumina.Data;
+using Lumina.Text;
+
+namespace DailyRoutines.Infos;
+
+[Sheet("leve/CraftLeveClient")]
+public class CraftLeveClient : ExcelRow
+{
+    public SeString? Name { get; set; }
+    public SeString? Text { get; set; }
+
+    public override void PopulateData(RowParser parser, GameData gameData, Language language)
+    {
+        base.PopulateData(parser, gameData, language);
+
+        Name = parser.ReadColumn<SeString>(0);
+        Text = parser.ReadColumn<SeString>(1);
+    }
+}
