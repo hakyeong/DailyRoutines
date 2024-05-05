14a5cebc145244c4c7cc9eafd3e6dcacbbf9a83a
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2024-05-05 21:58:58 +0800

    宏进入技能队列

---
+++ DailyRoutines/Modules/CombatExpand/MacroIntoActionQueue.cs
@@ -0,0 +1,27 @@
+using DailyRoutines.Infos;
+using DailyRoutines.Managers;
+using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.Game;
+
+namespace DailyRoutines.Modules;
+
+[ModuleDescription("MacroIntoActionQueueTitle", "MacroIntoActionQueueDescription", ModuleCategories.Combat)]
+public unsafe class MacroIntoActionQueue : DailyModuleBase
+{
+    private delegate bool UseActionMacroDelegate(ActionManager* actionManager, uint actionType, uint actionID, ulong targetID = 0xE000_0000, uint a4 = 0, uint a5 = 0, uint a6 = 0, void* a7 = null);
+    [Signature("E8 ?? ?? ?? ?? 80 7C 24 ?? ?? 44 0F B6 E8", DetourName = nameof(UseActionMacroDetour))]
+    private readonly Hook<UseActionMacroDelegate>? UseActionMacroHook;
+
+    public override void Init()
+    {
+        Service.Hook.InitializeFromAttributes(this);
+        UseActionMacroHook?.Enable();
+    }
+
+    private bool UseActionMacroDetour(ActionManager* actionManager, uint actionType, uint actionID, ulong targetID, uint a4, uint a5, uint a6, void* a7)
+    {
+        if (a5 == 2) a5 = 1;
+        return UseActionMacroHook.Original(actionManager, actionType, actionID, targetID, a4, a5, a6, a7);
+    }
+}
