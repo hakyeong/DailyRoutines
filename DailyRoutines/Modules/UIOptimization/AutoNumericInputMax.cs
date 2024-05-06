f476ef47e9d786d7ea9becb165208cf65af61c6c
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2024-05-06 21:00:54 +0800

    自动数字输入框最大值

---
+++ DailyRoutines/Modules/UIOptimization/AutoNumericInputMax.cs
@@ -0,0 +1,68 @@
+using DailyRoutines.Infos;
+using DailyRoutines.Managers;
+using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
+using ECommons.Throttlers;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System;
+
+namespace DailyRoutines.Modules;
+
+[ModuleDescription("AutoNumericInputMaxTitle", "AutoNumericInputMaxDescription", ModuleCategories.界面优化)]
+public unsafe class AutoNumericInputMax : DailyModuleBase
+{
+    private delegate nint InitFromComponentDataDelegate(AtkComponentNumericInput* component, AtkUldComponentDataNumericInput* data);
+    [Signature("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 0F 10 02 48 8B D9 0F 11 81", DetourName = nameof(InitFromComponentDataDetour))]
+    private readonly Hook<InitFromComponentDataDelegate>? InitFromComponentDataHook;
+
+    private delegate nint UldUpdateDelegate(AtkComponentNumericInput* component);
+    [Signature("40 53 48 83 EC ?? 0F B6 81 ?? ?? ?? ?? 48 8B D9 48 83 C1 ?? A8 ?? 74 ?? 48 83 79 ?? ?? 74 ?? A8 ?? 75 ?? 48 83 79 ?? ?? 75 ?? E8 ?? ?? ?? ?? EB ?? E8 ?? ?? ?? ?? 80 BB ?? ?? ?? ?? ?? 74 ?? 48 83 BB", DetourName = nameof(UldUpdateDetour))]
+    private readonly Hook<UldUpdateDelegate>? UldUpdateHook;
+
+    private static long _LastInterruptTime;
+
+    public override void Init()
+    {
+        Service.Hook.InitializeFromAttributes(this);
+        InitFromComponentDataHook?.Enable();
+        UldUpdateHook?.Enable();
+    }
+
+    public override void ConfigUI()
+    {
+        ConflictKeyText();
+
+        ImGuiOm.HelpMarker(Service.Lang.GetText("AutoNumericInputMax-InterruptHelp"));
+    }
+
+    private nint InitFromComponentDataDetour(AtkComponentNumericInput* component, AtkUldComponentDataNumericInput* data)
+    {
+        if (data->Max != 99999)
+        {
+            data->Value = data->Max;
+            component->SetValue(data->Max);
+        }
+        var result = InitFromComponentDataHook.Original(component, data);
+
+        // 一些界面初始化后还会再刷新
+        if (data->Max != 99999)
+            Service.Framework.RunOnTick(() => component->SetValue(data->Max), TimeSpan.FromMilliseconds(500));
+        return result;
+    }
+
+    private nint UldUpdateDetour(AtkComponentNumericInput* component)
+    {
+        var result = UldUpdateHook.Original(component);
+
+        // 一些界面切换 Tab 后也会刷新输入状态
+        if (Service.KeyState[Service.Config.ConflictKey])
+            _LastInterruptTime = Environment.TickCount64;
+
+        if (Environment.TickCount64 - _LastInterruptTime > 10000)
+            if (EzThrottler.Throttle($"AutoNumericInputMax-UldUpdate_{(nint)component}", 100) &&
+                component->Data.Max != 99999)
+                component->SetValue(component->Data.Max);
+
+        return result;
+    }
+}
