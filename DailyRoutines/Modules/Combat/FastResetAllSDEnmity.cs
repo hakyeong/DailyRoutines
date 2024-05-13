49757f889f5af3baa8c785930983d11734cfc436
AtmoOmen <atmoomen2004@outlook.com>
2024-05-13 21:25:03 +0800

    快速重置所有木人仇恨

---
+++ DailyRoutines/Modules/Combat/FastResetAllSDEnmity.cs
@@ -0,0 +1,79 @@
+using System.Linq;
+using DailyRoutines.Infos;
+using DailyRoutines.Managers;
+using Dalamud.Game.ClientState.Objects.Enums;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Game.Command;
+using ImGuiNET;
+
+namespace DailyRoutines.Modules;
+
+[ModuleDescription("FastResetAllSDEnmityTitle", "FastResetAllSDEnmityDescription", ModuleCategories.战斗)]
+public class FastResetAllSDEnmity : DailyModuleBase
+{
+    private static bool IsAddCommand = true;
+
+    private const string Command = "resetallsd";
+
+    public override void Init()
+    {
+        AddConfig(nameof(IsAddCommand), true);
+        IsAddCommand = GetConfig<bool>(nameof(IsAddCommand));
+
+        Service.ExecuteCommandManager.Register(OnResetStrikingDummies);
+        if (IsAddCommand)
+        {
+            Service.CommandManager.AddSubCommand(Command, new CommandInfo(OnCommand)
+            {
+                HelpMessage = Service.Lang.GetText("FastResetAllSDEnmity-CommandHelp")
+            });
+        }
+    }
+
+    public override void ConfigUI()
+    {
+        if (ImGui.Checkbox(
+                $"{Service.Lang.GetText("FastResetAllSDEnmity-AddCommand", Command)}: {Service.Lang.GetText("FastResetAllSDEnmity-CommandHelp")}",
+                ref IsAddCommand))
+        {
+            UpdateConfig(nameof(IsAddCommand), IsAddCommand);
+            if (IsAddCommand)
+            {
+                Service.CommandManager.AddSubCommand(Command, new CommandInfo(OnCommand)
+                {
+                    HelpMessage = Service.Lang.GetText("FastResetAllSDEnmity-CommandHelp")
+                });
+            }
+            else
+                Service.CommandManager.RemoveSubCommand(Command);
+        }
+    }
+
+    private static void OnCommand(string command, string arguments)
+    {
+        ResetAllStrikingDummies();
+    }
+
+    public static void OnResetStrikingDummies(int command, int objectID, int param2, int param3, int param4)
+    {
+        if (command != 319) return;
+
+        ResetAllStrikingDummies();
+    }
+
+    private static void ResetAllStrikingDummies()
+    {
+        foreach (var strikingDummy in Service.ObjectTable.Where(x => x.ObjectKind == ObjectKind.BattleNpc &&
+                                                                     x.TargetObject != null &&
+                                                                     (x as BattleChara).NameId == 541))
+            Service.ExecuteCommandManager.ExecuteCommand(319, (int)strikingDummy.ObjectId, 0, 0, 0);
+    }
+
+    public override void Uninit()
+    {
+        Service.ExecuteCommandManager.Unregister(OnResetStrikingDummies);
+        Service.CommandManager.RemoveSubCommand(Command);
+
+        base.Uninit();
+    }
+}
