26347f55c90085fb82269707e370daf92cb7cef0
AtmoOmen <atmoomen2004@outlook.com>
2024-05-13 17:02:27 +0800

    优化实现

--- DailyRoutines/Modules/Action/MacroIntoActionQueue.cs
+++ DailyRoutines/Modules/Action/MacroIntoActionQueue.cs
@@ -9,7 +9,7 @@ namespace DailyRoutines.Modules;
 [ModuleDescription("MacroIntoActionQueueTitle", "MacroIntoActionQueueDescription", ModuleCategories.技能)]
 public unsafe class MacroIntoActionQueue : DailyModuleBase
 {
-    private delegate bool UseActionMacroDelegate(ActionManager* actionManager, uint actionType, uint actionID, ulong targetID = 0xE000_0000, uint a4 = 0, uint a5 = 0, uint a6 = 0, void* a7 = null);
+    private delegate bool UseActionMacroDelegate(ActionManager* actionManager, ActionType actionType, uint actionID, ulong targetID = 0xE000_0000, uint a4 = 0, uint a5 = 0, uint a6 = 0, void* a7 = null);
     [Signature("E8 ?? ?? ?? ?? 80 7C 24 ?? ?? 44 0F B6 E8", DetourName = nameof(UseActionMacroDetour))]
     private readonly Hook<UseActionMacroDelegate>? UseActionMacroHook;
 
@@ -19,9 +19,17 @@ public unsafe class MacroIntoActionQueue : DailyModuleBase
         UseActionMacroHook?.Enable();
     }
 
-    private bool UseActionMacroDetour(ActionManager* actionManager, uint actionType, uint actionID, ulong targetID, uint a4, uint a5, uint a6, void* a7)
+    private bool UseActionMacroDetour(ActionManager* actionManager, ActionType actionType, uint actionID, ulong targetID, uint a4, uint queueState, uint a6, void* a7)
     {
-        if (a5 == 2) a5 = 1;
-        return UseActionMacroHook.Original(actionManager, actionType, actionID, targetID, a4, a5, a6, a7);
+        // 宏
+        if (queueState is 0 or 2) queueState = 1;
+        // 冲刺
+        if (actionType == ActionType.GeneralAction && actionID == 4)
+        {
+            actionType = ActionType.Action;
+            actionID = 3;
+            targetID = 0xE000_0000;
+        }
+        return UseActionMacroHook.Original(actionManager, actionType, actionID, targetID, a4, queueState, a6, a7);
     }
 }
