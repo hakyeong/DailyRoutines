9882c6496f2bf1a08377f5a8bcc9d01764a7263e
AtmoOmen <atmoomen2004@outlook.com>
2024-05-17 22:04:54 +0800

    Update PresetData.cs

--- DailyRoutines/Infos/PresetData.cs
+++ DailyRoutines/Infos/PresetData.cs
@@ -20,8 +20,10 @@ public class PresetData
     public static void Init()
     {
         PlayerActions ??= LuminaCache.Get<Action>()
-                                     .Where(x => x.ClassJob.Value != null && x.Range != -1 && x.Icon != 0 &&
-                                                 !string.IsNullOrWhiteSpace(x.Name.RawString))
+                                     .Where(x => x.ClassJob.Value != null && x.Range != -1 &&
+                                                 x.Icon != 0 && !string.IsNullOrWhiteSpace(x.Name.RawString))
+                                     .Where(x => x is { IsPlayerAction: false, ClassJobLevel: > 0 } 
+                                                   or { IsPlayerAction: true })
                                      .OrderBy(x => x.ClassJob.Row)
                                      .ThenBy(x => x.ClassJobLevel)
                                      .ToDictionary(x => x.RowId, x => x);
