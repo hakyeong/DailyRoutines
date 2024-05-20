e3db5d31631f9e91a060cf23f287b8cd18af740e
AtmoOmen <atmoomen2004@outlook.com>
2024-05-20 19:18:52 +0800

    Update Debug.cs

--- DailyRoutines/Windows/Debug.cs
+++ DailyRoutines/Windows/Debug.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Linq;
+using System.Reflection;
+using DailyRoutines.Infos;
 using DailyRoutines.Managers;
+using DailyRoutines.Modules;
 using Dalamud.Interface.Colors;
 using Dalamud.Interface.Utility;
 using Dalamud.Interface.Windowing;
@@ -22,6 +26,47 @@ public class Debug() : Window("Daily Routines - 调试窗口", ImGuiWindowFlags.
 
     public override void Draw()
     {
+        if (ImGui.Button("输出模块一览"))
+        {
+            var allModules = Assembly.GetExecutingAssembly().GetTypes()
+                                     .Where(t => typeof(DailyModuleBase).IsAssignableFrom(t) &&
+                                                 t is { IsClass: true, IsAbstract: false } &&
+                                                 t.GetCustomAttribute<ModuleDescriptionAttribute>() != null)
+                                     .Select(type => new
+                                     {
+                                         Title = Service.Lang.GetText(
+                                             type.GetCustomAttribute<ModuleDescriptionAttribute>()?.TitleKey ??
+                                             "DevModuleTitle"),
+                                         Description = Service.Lang.GetText(
+                                             type.GetCustomAttribute<ModuleDescriptionAttribute>()?.DescriptionKey ??
+                                             "DevModuleDescription"),
+                                         Category = type.GetCustomAttribute<ModuleDescriptionAttribute>()?.Category ??
+                                                    ModuleCategories.一般,
+                                     })
+                                     .OrderBy(m => m.Category)
+                                     .ToList();
+
+            var groupedModules = allModules.GroupBy(m => m.Category);
+
+            var markdown = "";
+            foreach (var group in groupedModules)
+            {
+                markdown += $"## {group.Key}\n";
+                markdown += "| 名称 | 描述 |\n";
+                markdown += "|------|------|\n";
+
+                foreach (var module in group)
+                {
+                    var formattedDescription = module.Description.Replace("\n", "<br>");
+                    markdown += $"| {module.Title} | {formattedDescription} |\n";
+                }
+
+                markdown += "\n";
+            }
+
+            ImGui.SetClipboardText(markdown);
+        }
+
         ImGui.PushID("Execute Command Manager");
         ImGui.TextColored(ImGuiColors.DalamudYellow, "Execute Command Manager");
         ImGui.Separator();
