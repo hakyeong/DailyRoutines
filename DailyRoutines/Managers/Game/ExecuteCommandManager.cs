be3dc639ad78a810d6db67ce3a44f0c40ca41414
AtmoOmen <atmoomen2004@outlook.com>
2024-05-21 09:01:44 +0800

    Update ExecuteCommandManager.cs

--- DailyRoutines/Managers/Game/ExecuteCommandManager.cs
+++ DailyRoutines/Managers/Game/ExecuteCommandManager.cs
@@ -2,7 +2,6 @@ using DailyRoutines.Infos;
 using Dalamud.Hooking;
 using Dalamud.Utility.Signatures;
 using System.Collections.Generic;
-using System.Reflection;
 using DailyRoutines.Windows;
 
 namespace DailyRoutines.Managers;
@@ -13,16 +12,25 @@ public class ExecuteCommandManager : IDailyManager
     [Signature("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? ?? ?? ?? 8B E9 41 8B D9 48 8B 0D ?? ?? ?? ?? 41 8B F8 8B F2", DetourName = nameof(ExecuteCommandDetour))]
     private static Hook<ExecuteCommandDelegate>? ExecuteCommandHook;
 
+    public delegate void PreExecuteCommandDelegate(
+        ref int command, ref int param1, ref int param2, ref int param3, ref int param4);
     public delegate void ExecuteCommandReceivedDelegate(int command, int param1, int param2, int param3, int param4);
 
-    private static Dictionary<string, ExecuteCommandReceivedDelegate>? MethodsInfo;
-    private static ExecuteCommandReceivedDelegate[]? _methods;
-    private static int _length;
+    private static Dictionary<string, ExecuteCommandReceivedDelegate>? MethodsInfoReceive;
+    private static ExecuteCommandReceivedDelegate[]? _methodsReceive;
+    private static int _lengthReceive;
+
+    private static Dictionary<string, PreExecuteCommandDelegate>? MethodsInfoPre;
+    private static PreExecuteCommandDelegate[]? _methodsPre;
+    private static int _lengthPre;
 
     private void Init()
     {
-        MethodsInfo ??= [];
-        _methods ??= [];
+        MethodsInfoReceive ??= [];
+        _methodsReceive ??= [];
+
+        MethodsInfoPre ??= [];
+        _methodsPre ??= [];
 
         Service.Hook.InitializeFromAttributes(this);
         ExecuteCommandHook?.Enable();
@@ -36,21 +44,22 @@ public class ExecuteCommandManager : IDailyManager
 
     private static nint ExecuteCommandDetour(int command, int param1, int param2, int param3, int param4)
     {
-        var original = ExecuteCommandHook.Original(command, param1, param2, param3, param4);
-
         if (Debug.DebugConfig.ShowExecuteCommandLog)
             Service.Log.Debug($"[Execute Command Manager]\n命令:{command} | p1:{param1} | p2:{param2} | p3:{param3} | p4:{param4}");
 
-        OnExecuteCommand(command, param1, param2, param3, param4);
-        return original;
-    }
-
-    private static void OnExecuteCommand(int command, int param1, int param2, int param3, int param4)
-    {
-        for (var i = 0; i < _length; i++)
+        for (var i = 0; i < _lengthPre; i++)
         {
-            _methods[i].Invoke(command, param1, param2, param3, param4);
+            _methodsPre[i].Invoke(ref command, ref param1, ref param2, ref param3, ref param4);
         }
+
+        var original = ExecuteCommandHook.Original(command, param1, param2, param3, param4);
+
+        for (var i = 0; i < _lengthReceive; i++)
+        {
+            _methodsReceive[i].Invoke(command, param1, param2, param3, param4);
+        }
+
+        return original;
     }
 
     public bool Register(params ExecuteCommandReceivedDelegate[] methods)
@@ -59,7 +68,20 @@ public class ExecuteCommandManager : IDailyManager
         foreach (var method in methods)
         {
             var uniqueName = GetUniqueName(method);
-            if (!MethodsInfo.TryAdd(uniqueName, method)) state = false;
+            if (!MethodsInfoReceive.TryAdd(uniqueName, method)) state = false;
+        }
+
+        UpdateMethodsArray();
+        return state;
+    }
+
+    public bool Register(params PreExecuteCommandDelegate[] methods)
+    {
+        var state = true;
+        foreach (var method in methods)
+        {
+            var uniqueName = GetUniqueName(method);
+            if (!MethodsInfoPre.TryAdd(uniqueName, method)) state = false;
         }
 
         UpdateMethodsArray();
@@ -72,20 +94,20 @@ public class ExecuteCommandManager : IDailyManager
         foreach (var method in methods)
         {
             var uniqueName = GetUniqueName(method);
-            if (!MethodsInfo.Remove(uniqueName)) state = false;
+            if (!MethodsInfoReceive.Remove(uniqueName)) state = false;
         }
 
         UpdateMethodsArray();
         return state;
     }
 
-    public bool Unregister(params MethodInfo[] methods)
+    public bool Unregister(params PreExecuteCommandDelegate[] methods)
     {
         var state = true;
         foreach (var method in methods)
         {
             var uniqueName = GetUniqueName(method);
-            if (!MethodsInfo.Remove(uniqueName)) state = false;
+            if (!MethodsInfoPre.Remove(uniqueName)) state = false;
         }
 
         UpdateMethodsArray();
@@ -98,15 +120,18 @@ public class ExecuteCommandManager : IDailyManager
         return $"{methodInfo.DeclaringType.FullName}_{methodInfo.Name}";
     }
 
-    private static string GetUniqueName(MemberInfo methodInfo)
+    private static string GetUniqueName(PreExecuteCommandDelegate method)
     {
+        var methodInfo = method.Method;
         return $"{methodInfo.DeclaringType.FullName}_{methodInfo.Name}";
     }
 
     private static void UpdateMethodsArray()
     {
-        _methods = [.. MethodsInfo.Values];
-        _length = _methods.Length;
+        _methodsReceive = [.. MethodsInfoReceive.Values];
+        _lengthReceive = _methodsReceive.Length;
+        _methodsPre = [.. MethodsInfoPre.Values];
+        _lengthPre = _methodsPre.Length;
     }
 
     private void Uninit()
