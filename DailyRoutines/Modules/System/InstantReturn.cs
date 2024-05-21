a1197321fccbeb803c28800bf39cbc630362f9d1
AtmoOmen <atmoomen2004@outlook.com>
2024-05-21 23:11:39 +0800

    即刻返回

---
+++ DailyRoutines/Modules/System/InstantReturn.cs
@@ -0,0 +1,27 @@
+using DailyRoutines.Infos;
+using DailyRoutines.Managers;
+using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
+
+namespace DailyRoutines.Modules;
+
+[ModuleDescription("InstantReturnTitle", "InstantReturnDescription", ModuleCategories.系统)]
+public class InstantReturn : DailyModuleBase
+{
+    private delegate byte ReturnDelegate(nint a1);
+
+    [Signature("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 48 8B 3D ?? ?? ?? ?? 48 8B D9 48 8D 0D", DetourName = nameof(ReturnDetour))]
+    private static Hook<ReturnDelegate>? ReturnHook;
+
+    public override void Init()
+    {
+        Service.Hook.InitializeFromAttributes(this);
+        ReturnHook?.Enable();
+    }
+
+    private static byte ReturnDetour(nint _)
+    {
+        Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.InstantReturn, 0, 0, 0, 0);
+        return 1;
+    }
+}
