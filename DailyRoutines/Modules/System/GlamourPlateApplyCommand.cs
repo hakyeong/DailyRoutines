677ebef02adce37047ee287bbf315fc6b1522761
AtmoOmen <atmoomen2004@outlook.com>
2024-05-23 09:12:05 +0800

    GlamourPlateApplyCommand

---
+++ DailyRoutines/Modules/System/GlamourPlateApplyCommand.cs
@@ -0,0 +1,46 @@
+using System;
+using DailyRoutines.Infos;
+using DailyRoutines.Managers;
+using Dalamud.Game.Command;
+using FFXIVClientStructs.FFXIV.Client.Game;
+
+namespace DailyRoutines.Modules;
+
+[ModuleDescription("GlamourPlateApplyCommandTitle", "GlamourPlateApplyCommandDescription", ModuleCategories.系统)]
+public unsafe class GlamourPlateApplyCommand : DailyModuleBase
+{
+    private const string Command = "gpapply";
+
+    public override void Init()
+    {
+        Service.CommandManager.AddSubCommand(Command, new CommandInfo(OnCommand));
+    }
+
+    private static void OnCommand(string command, string arguments)
+    {
+        if (string.IsNullOrWhiteSpace(arguments) || 
+            !int.TryParse(arguments.Trim(), out var index) || index is < 1 or > 20) return;
+
+        var mirageManager = MirageManager.Instance();
+        if (!mirageManager->GlamourPlatesLoaded)
+        {
+            Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.RequestGlamourPlates);
+            Service.Framework.RunOnTick(() => ApplyGlamourPlate(index), TimeSpan.FromMilliseconds(500));
+            return;
+        }
+
+        ApplyGlamourPlate(index);
+    }
+
+    private static void ApplyGlamourPlate(int index)
+    {
+        Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.EnterGlamourPlateState, 1, 1);
+        Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.ApplyGlamourPlate, index - 1);
+        Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.EnterGlamourPlateState, 0, 1);
+    }
+
+    public override void Uninit()
+    {
+        Service.CommandManager.RemoveSubCommand(Command);
+    }
+}
