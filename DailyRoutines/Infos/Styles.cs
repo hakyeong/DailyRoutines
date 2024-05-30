3f079ee7c8fd424b83b50a567240f2dd14f2b705
AtmoOmen <atmoomen2004@outlook.com>
2024-05-30 16:47:19 +0800

    RadioButton

--- DailyRoutines/Infos/Styles.cs
+++ DailyRoutines/Infos/Styles.cs
@@ -6,6 +6,7 @@ namespace DailyRoutines.Infos;
 public static class Styles
 {
     public static Vector2 CheckboxSize { get; set; }
+    public static Vector2 RadioButtionSize { get; set; }
 
     public static void Refresh()
     {
@@ -14,5 +15,8 @@ public static class Styles
         var checkboxSizeBool = false;
         ImGui.Checkbox("", ref checkboxSizeBool);
         CheckboxSize = ImGui.GetItemRectSize() - style.FramePadding / 2;
+
+        ImGui.RadioButton("", false);
+        RadioButtionSize = ImGui.GetItemRectSize() - style.FramePadding / 2;
     }
 }
