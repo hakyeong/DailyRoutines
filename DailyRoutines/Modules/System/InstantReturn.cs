b51c1a9d51d6ca557a5d69c5a16aa1de8167f7a2
decorwdyun <decorwdyun@gmail.com>
2024-06-04 18:37:35 +0800

    feat: add instant return command

--- DailyRoutines/Modules/System/InstantReturn.cs
+++ DailyRoutines/Modules/System/InstantReturn.cs
@@ -1,8 +1,10 @@
 using DailyRoutines.Infos;
 using DailyRoutines.Managers;
+using Dalamud.Game.Command;
 using Dalamud.Hooking;
 using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using ImGuiNET;
 
 namespace DailyRoutines.Modules;
 
@@ -13,15 +15,70 @@ public class InstantReturn : DailyModuleBase
                DetourName = nameof(ReturnDetour))]
     private static Hook<ReturnDelegate>? ReturnHook;
 
+    private delegate byte ReturnDelegate(nint a1);
+
+    private const string Command = "instantreturn";
+    private static bool IsAddCommand;
+    private static bool HookOrigin = true;
+
     public override void Init()
     {
+        AddConfig(nameof(IsAddCommand), false);
+        IsAddCommand = GetConfig<bool>(nameof(IsAddCommand));
+
+        AddConfig(nameof(HookOrigin), true);
+        HookOrigin = GetConfig<bool>(nameof(HookOrigin));
+    
         Service.Hook.InitializeFromAttributes(this);
         ReturnHook?.Enable();
+
+        if (IsAddCommand)
+        {
+            Service.CommandManager.AddSubCommand(Command, new CommandInfo(OnCommand)
+            {
+                HelpMessage = Service.Lang.GetText("InstantReturn-CommandHelp"),
+            });
+        }
+    }
+
+    public override void ConfigUI()
+    {
+        if (ImGui.Checkbox(
+                $"{Service.Lang.GetText("InstantReturn-HookOrigin")}",
+                ref HookOrigin))
+        {
+            UpdateConfig(nameof(HookOrigin), HookOrigin);
+        }
+
+        if (ImGui.Checkbox(
+                $"{Service.Lang.GetText("InstantReturn-AddCommand", Command)}: {Service.Lang.GetText("InstantReturn-CommandHelp")}",
+                ref IsAddCommand))
+        {
+            UpdateConfig(nameof(IsAddCommand), IsAddCommand);
+            if (IsAddCommand)
+            {
+                Service.CommandManager.AddSubCommand(Command, new CommandInfo(OnCommand)
+                {
+                    HelpMessage = Service.Lang.GetText("InstantReturn-CommandHelp"),
+                });
+            }
+            else
+            {
+                Service.CommandManager.RemoveSubCommand(Command);
+            }
+        }
+    }
+
+    private void OnCommand(string command, string arguments)
+    {
+        if (Service.ClientState.IsPvPExcludingDen) return;
+        Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.InstantReturn);
     }
 
     private static unsafe byte ReturnDetour(nint a1)
     {
-        if (Service.ClientState.IsPvPExcludingDen ||
+        if (!HookOrigin ||
+            Service.ClientState.IsPvPExcludingDen ||
             ActionManager.Instance()->GetActionStatus(ActionType.GeneralAction, 6) != 0)
             return ReturnHook.Original(a1);
 
@@ -29,5 +86,10 @@ public class InstantReturn : DailyModuleBase
         return 1;
     }
 
-    private delegate byte ReturnDelegate(nint a1);
+    public override void Uninit()
+    {
+        ReturnHook?.Dispose();
+        Service.CommandManager.RemoveSubCommand(Command);
+        base.Uninit();
+    }
 }
