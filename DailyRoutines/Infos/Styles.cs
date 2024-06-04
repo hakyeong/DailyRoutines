a67bb6ca6e52ebd01d967d8c8d2854dcf91d24e7
AtmoOmen <atmoomen2004@outlook.com>
2024-06-04 22:17:05 +0800

    Update Styles.cs

--- DailyRoutines/Infos/Styles.cs
+++ DailyRoutines/Infos/Styles.cs
@@ -11,25 +11,14 @@ public static class Styles
 
     public static void Refresh()
     {
-        var isOpen = true;
-        ImGui.SetWindowSize(Vector2.Zero);
-        ImGui.SetNextWindowSizeConstraints(Vector2.Zero, Vector2.One);
-        ImGui.SetNextWindowSize(Vector2.Zero);
-        ImGui.SetNextWindowContentSize(Vector2.Zero);
-        ImGui.SetNextWindowPos(new(25000f * ImGuiHelpers.GlobalScale));
-        if (ImGui.Begin("###DailyRoutines-Styles", ref isOpen, ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoBringToFrontOnFocus))
-        {
-            ImGui.CloseCurrentPopup();
-            
-            var style = ImGui.GetStyle();
+        ImGui.SetWindowPos(new(25000f));
+        var style = ImGui.GetStyle();
 
-            var checkboxSizeBool = false;
-            ImGui.Checkbox("", ref checkboxSizeBool);
-            CheckboxSize = ImGui.GetItemRectSize() - style.FramePadding / 2;
+        var checkboxSizeBool = false;
+        ImGui.Checkbox("", ref checkboxSizeBool);
+        CheckboxSize = ImGui.GetItemRectSize() - style.FramePadding / 2;
 
-            ImGui.RadioButton("", false);
-            RadioButtionSize = ImGui.GetItemRectSize() - style.FramePadding / 2;
-            ImGui.End();
-        }
+        ImGui.RadioButton("", false);
+        RadioButtionSize = ImGui.GetItemRectSize() - style.FramePadding / 2;
     }
 }
