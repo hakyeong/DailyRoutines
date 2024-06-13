16cfe91b81bafc5d183af068549304a66d220d25
AtmoOmen <atmoomen2004@outlook.com>
2024-06-13 20:46:34 +0800

    Create AddonState.cs

---
+++ DailyRoutines/Infos/AddonState.cs
@@ -0,0 +1,15 @@
+using DailyRoutines.Managers;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace DailyRoutines.Infos;
+
+public static unsafe class AddonState
+{
+    public static AtkUnitBase* Inventory          => (AtkUnitBase*)Service.Gui.GetAddonByName("Inventory");
+    public static AtkUnitBase* InventoryLarge     => (AtkUnitBase*)Service.Gui.GetAddonByName("InventoryLarge");
+    public static AtkUnitBase* InventoryExpansion => (AtkUnitBase*)Service.Gui.GetAddonByName("InventoryExpansion");
+    public static AtkUnitBase* SelectString       => (AtkUnitBase*)Service.Gui.GetAddonByName("SelectString");
+    public static AtkUnitBase* InputNumeric       => (AtkUnitBase*)Service.Gui.GetAddonByName("InputNumeric");
+
+    public static AtkUnitBase* ToAtkUnitBase(this nint ptr) => (AtkUnitBase*)ptr;
+}
