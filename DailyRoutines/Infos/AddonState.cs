e628a74cf0c5687e274aadf45bf3c4ce13b05b34
AtmoOmen <atmoomen2004@outlook.com>
2024-06-14 16:14:07 +0800

    Update AddonState.cs

--- DailyRoutines/Infos/AddonState.cs
+++ DailyRoutines/Infos/AddonState.cs
@@ -5,12 +5,18 @@ namespace DailyRoutines.Infos;
 
 public static unsafe class AddonState
 {
-    public static AtkUnitBase* Inventory          => (AtkUnitBase*)Service.Gui.GetAddonByName("Inventory");
-    public static AtkUnitBase* InventoryLarge     => (AtkUnitBase*)Service.Gui.GetAddonByName("InventoryLarge");
-    public static AtkUnitBase* InventoryExpansion => (AtkUnitBase*)Service.Gui.GetAddonByName("InventoryExpansion");
-    public static AtkUnitBase* SelectString       => (AtkUnitBase*)Service.Gui.GetAddonByName("SelectString");
-    public static AtkUnitBase* InputNumeric       => (AtkUnitBase*)Service.Gui.GetAddonByName("InputNumeric");
-    public static AtkUnitBase* ChatLog            => (AtkUnitBase*)Service.Gui.GetAddonByName("ChatLog");
+    public static AtkUnitBase* Inventory           => (AtkUnitBase*)Service.Gui.GetAddonByName("Inventory");
+    public static AtkUnitBase* InventoryLarge      => (AtkUnitBase*)Service.Gui.GetAddonByName("InventoryLarge");
+    public static AtkUnitBase* InventoryExpansion  => (AtkUnitBase*)Service.Gui.GetAddonByName("InventoryExpansion");
+    public static AtkUnitBase* SelectString        => (AtkUnitBase*)Service.Gui.GetAddonByName("SelectString");
+    public static AtkUnitBase* InputNumeric        => (AtkUnitBase*)Service.Gui.GetAddonByName("InputNumeric");
+    public static AtkUnitBase* ChatLog             => (AtkUnitBase*)Service.Gui.GetAddonByName("ChatLog");
+    public static AtkUnitBase* SelectYesno         => (AtkUnitBase*)Service.Gui.GetAddonByName("SelectYesno");
+    public static AtkUnitBase* TitleMenu           => (AtkUnitBase*)Service.Gui.GetAddonByName("_TitleMenu");
+    public static AtkUnitBase* CharaSelectListMenu => (AtkUnitBase*)Service.Gui.GetAddonByName("_CharaSelectListMenu");
+
+    public static AtkUnitBase* CharaSelectWorldServer =>
+        (AtkUnitBase*)Service.Gui.GetAddonByName("_CharaSelectWorldServer");
 
     public static AtkUnitBase* ToAtkUnitBase(this nint ptr) => (AtkUnitBase*)ptr;
 }
