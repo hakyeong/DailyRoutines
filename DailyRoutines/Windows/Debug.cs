6a87ac6409a69a5117069745077b1772c1886877
AtmoOmen <atmoomen2004@outlook.com>
2024-06-16 20:28:15 +0800

    Update Debug.cs

--- DailyRoutines/Windows/Debug.cs
+++ DailyRoutines/Windows/Debug.cs
@@ -1,17 +1,24 @@
 using System;
 using System.Linq;
+using System.Numerics;
 using System.Reflection;
+using DailyRoutines.Helpers;
+using DailyRoutines.Infos;
 using DailyRoutines.Managers;
 using DailyRoutines.Modules;
-using Dalamud.Interface;
 using Dalamud.Interface.Colors;
 using Dalamud.Interface.Utility;
+using Dalamud.Interface.Utility.Raii;
 using Dalamud.Interface.Windowing;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using ImGuiNET;
+using Lumina.Excel.GeneratedSheets;
+using Map = Lumina.Excel.GeneratedSheets.Map;
 
 namespace DailyRoutines.Windows;
 
-public class Debug() : Window("Daily Routines - 调试窗口###DailyRoutines-Debug", ImGuiWindowFlags.AlwaysAutoResize), IDisposable
+public unsafe class Debug() : Window("Daily Routines - 调试窗口###DailyRoutines-Debug", ImGuiWindowFlags.AlwaysAutoResize), IDisposable
 {
     internal class Config
     {
@@ -27,54 +34,211 @@ public class Debug() : Window("Daily Routines - 调试窗口###DailyRoutines-Deb
 
     public override void Draw()
     {
-        ImGui.PushID("快捷操作");
-        ImGui.TextColored(ImGuiColors.DalamudYellow, "快捷操作:");
-        ImGui.Separator();
-
-        if (ImGui.Button("输出模块一览"))
+        if (!PresetFont.Axis14.Available) return;
+        using var font = ImRaii.PushFont(PresetFont.Axis14.Lock().ImFont);
+        try
         {
-            var markdown = OutputAllModulesMarkdown();
-            ImGui.SetClipboardText(markdown);
+            if (ImGui.BeginTabBar("DebugTab"))
+            {
+                DrawInfo();
+                DrawManagers();
+                DrawOthers();
+                ImGui.EndTabBar();
+            }
         }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
 
-        ImGui.PopID();
+    private static void DrawInfo()
+    {
+        if (ImGui.BeginTabItem("信息"))
+        {
+            var infoInstance = TerritoryInfo.Instance();
+            var mapAgent = AgentMap.Instance();
+            if (ImGui.BeginTable("TerritoryInfoTable", 2, ImGuiTableFlags.Borders))
+            {
+                ImGui.TableNextRow(ImGuiTableRowFlags.Headers);
+                ImGui.TableNextColumn();
+                ImGui.Text("区域信息");
 
-        ImGui.PushID("Execute Command Manager");
-        ImGui.TextColored(ImGuiColors.DalamudYellow, "Execute Command Manager");
-        ImGui.Separator();
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Territory Type");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{Service.ClientState.TerritoryType} ({LuminaCache.GetRow<TerritoryType>(Service.ClientState.TerritoryType)?.ExtractPlaceName() ?? ""})");
 
-        ImGui.Checkbox("显示日志###Execute Command Manager", ref DebugConfig.ShowExecuteCommandLog);
+                ImGui.TableNextRow(ImGuiTableRowFlags.Headers);
+                ImGui.TableNextColumn();
+                ImGui.Text("地图信息");
 
-        ImGui.PushItemWidth(70f * ImGuiHelpers.GlobalScale);
-        ImGui.InputInt("Command", ref ExecuteCommandManual.commmand, 0, 0);
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Map ID");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{Service.ClientState.MapId} ({LuminaCache.GetRow<Map>(Service.ClientState.MapId)?.PlaceName.Value.Name ?? ""})");
 
-        ImGui.InputInt("P1", ref ExecuteCommandManual.p1, 0, 0);
-        ImGui.SameLine();
-        ImGui.InputInt("P2", ref ExecuteCommandManual.p2, 0, 0);
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Map Size Factor");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{mapAgent->CurrentMapSizeFactorFloat}");
 
-        ImGui.InputInt("P3", ref ExecuteCommandManual.p3, 0, 0);
-        ImGui.SameLine();
-        ImGui.InputInt("P4", ref ExecuteCommandManual.p4, 0, 0);
-        ImGui.PopItemWidth();
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Map Offset");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{new Vector2(mapAgent->CurrentOffsetX, mapAgent->CurrentOffsetY)}");
 
-        ImGui.SameLine();
-        if (ImGui.Button("执行"))
-            Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandManual.commmand, ExecuteCommandManual.p1,
-                                                         ExecuteCommandManual.p2, ExecuteCommandManual.p3,
-                                                         ExecuteCommandManual.p4);
-        ImGui.PopID();
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Selected Map ID");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{mapAgent->SelectedMapId}");
 
-        ImGui.TextColored(ImGuiColors.DalamudYellow, "Log Message Manager");
-        ImGui.Separator();
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Selected Map Size Factor");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{mapAgent->SelectedMapSizeFactorFloat}");
 
-        ImGui.Checkbox("显示日志###Log Message Manager", ref DebugConfig.ShowLogMessageLog);
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Selected Map Offset");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{new Vector2(mapAgent->SelectedOffsetX, mapAgent->SelectedOffsetY)}");
 
-        ImGui.TextColored(ImGuiColors.DalamudYellow, "Use Action Manager");
-        ImGui.Separator();
+                ImGui.TableNextRow(ImGuiTableRowFlags.Headers);
+                ImGui.TableNextColumn();
+                ImGui.Text("标点信息");
 
-        ImGui.Checkbox("显示 Action 日志##Use Action Manager", ref DebugConfig.ShowUseActionLog);
-        ImGui.Checkbox("显示 Location Action 日志##Use Action Manager", ref DebugConfig.ShowUseActionLocationLog);
-        ImGui.Checkbox("显示 Pet Move 日志##Use Action Manager", ref DebugConfig.ShowUseActionPetMoveLog);
+                if (mapAgent->IsFlagMarkerSet == 1)
+                {
+                    ImGui.TableNextRow();
+                    ImGui.TableNextColumn();
+                    ImGui.Text("Position");
+                    ImGui.TableNextColumn();
+                    ImGui.Text($"{new Vector2(mapAgent->FlagMapMarker.XFloat, mapAgent->FlagMapMarker.YFloat)}");
+
+                    ImGui.TableNextRow();
+                    ImGui.TableNextColumn();
+                    ImGui.Text("Territory Type");
+                    ImGui.TableNextColumn();
+                    ImGui.Text($"{mapAgent->FlagMapMarker.TerritoryId} ({LuminaCache.GetRow<TerritoryType>(mapAgent->FlagMapMarker.TerritoryId)?.ExtractPlaceName() ?? ""})");
+
+                    ImGui.TableNextRow();
+                    ImGui.TableNextColumn();
+                    ImGui.Text("Map ID");
+                    ImGui.TableNextColumn();
+                    ImGui.Text($"{mapAgent->FlagMapMarker.MapId} ({LuminaCache.GetRow<Map>(Service.ClientState.MapId)?.PlaceName.Value.Name ?? ""})");
+                }
+
+                ImGui.TableNextRow(ImGuiTableRowFlags.Headers);
+                ImGui.TableNextColumn();
+                ImGui.Text("地区信息");
+
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Area PlaceName ID");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{infoInstance->AreaPlaceNameID}");
+
+                ImGui.TableNextColumn();
+                ImGui.Text("Area PlaceName Name");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{LuminaCache.GetRow<PlaceName>(infoInstance->AreaPlaceNameID).Name.RawString}");
+
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Sub-Area PlaceName ID");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{infoInstance->SubAreaPlaceNameID}");
+
+                ImGui.TableNextColumn();
+                ImGui.Text("Sub-Area PlaceName Name");
+                ImGui.TableNextColumn();
+                ImGui.Text($"{LuminaCache.GetRow<PlaceName>(infoInstance->SubAreaPlaceNameID).Name.RawString}");
+
+                ImGui.TableNextRow(ImGuiTableRowFlags.Headers);
+                ImGui.TableNextColumn();
+                ImGui.Text("位置信息");
+
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.Text("Position");
+                ImGui.TableNextColumn();
+                var roundedX = (float)Math.Round(Service.ClientState.LocalPlayer?.Position.X ?? 0f, 2);
+                var roundedY = (float)Math.Round(Service.ClientState.LocalPlayer?.Position.Y ?? 0f, 2);
+                var roundedZ = (float)Math.Round(Service.ClientState.LocalPlayer?.Position.Z ?? 0f, 2);
+                ImGui.Text($"<{roundedX:F2}, {roundedY:F2}, {roundedZ:F2}>");
+
+                ImGui.EndTable();
+            }
+
+            ImGui.EndTabItem();
+        }
+    }
+
+    private static void DrawManagers()
+    {
+        if (ImGui.BeginTabItem("管理器"))
+        {
+            ImGui.PushID("Execute Command Manager");
+            ImGui.TextColored(ImGuiColors.DalamudYellow, "Execute Command Manager");
+            ImGui.Separator();
+
+            ImGui.Checkbox("显示日志###Execute Command Manager", ref DebugConfig.ShowExecuteCommandLog);
+
+            ImGui.PushItemWidth(70f * ImGuiHelpers.GlobalScale);
+            ImGui.InputInt("Command", ref ExecuteCommandManual.commmand, 0, 0);
+
+            ImGui.InputInt("P1", ref ExecuteCommandManual.p1, 0, 0);
+            ImGui.SameLine();
+            ImGui.InputInt("P2", ref ExecuteCommandManual.p2, 0, 0);
+
+            ImGui.InputInt("P3", ref ExecuteCommandManual.p3, 0, 0);
+            ImGui.SameLine();
+            ImGui.InputInt("P4", ref ExecuteCommandManual.p4, 0, 0);
+            ImGui.PopItemWidth();
+
+            ImGui.SameLine();
+            if (ImGui.Button("执行"))
+                Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandManual.commmand, ExecuteCommandManual.p1,
+                                                             ExecuteCommandManual.p2, ExecuteCommandManual.p3,
+                                                             ExecuteCommandManual.p4);
+            ImGui.PopID();
+
+            ImGui.TextColored(ImGuiColors.DalamudYellow, "Log Message Manager");
+            ImGui.Separator();
+
+            ImGui.Checkbox("显示日志###Log Message Manager", ref DebugConfig.ShowLogMessageLog);
+
+            ImGui.TextColored(ImGuiColors.DalamudYellow, "Use Action Manager");
+            ImGui.Separator();
+
+            ImGui.Checkbox("显示 Action 日志##Use Action Manager", ref DebugConfig.ShowUseActionLog);
+            ImGui.Checkbox("显示 Location Action 日志##Use Action Manager", ref DebugConfig.ShowUseActionLocationLog);
+            ImGui.Checkbox("显示 Pet Move 日志##Use Action Manager", ref DebugConfig.ShowUseActionPetMoveLog);
+            ImGui.EndTabItem();
+        }
+    }
+
+    private static void DrawOthers()
+    {
+        if (ImGui.BeginTabItem("其他"))
+        {
+            ImGui.TextColored(ImGuiColors.DalamudYellow, "快捷操作:");
+            ImGui.Separator();
+
+            if (ImGui.Button("输出模块一览"))
+            {
+                var markdown = OutputAllModulesMarkdown();
+                ImGui.SetClipboardText(markdown);
+            }
+            ImGui.EndTabItem();
+        }
     }
 
     private static string OutputAllModulesMarkdown()
