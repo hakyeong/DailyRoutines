98aade2cfb0fd11278c74f18ce34a5d48d34371e
AtmoOmen <atmoomen2004@outlook.com>
2024-06-17 19:29:46 +0800

    Update AddonState.cs

--- DailyRoutines/Infos/AddonState.cs
+++ DailyRoutines/Infos/AddonState.cs
@@ -5,23 +5,21 @@ namespace DailyRoutines.Infos;
 
 public static unsafe class AddonState
 {
-    public static AtkUnitBase* Inventory           => (AtkUnitBase*)Service.Gui.GetAddonByName("Inventory");
-    public static AtkUnitBase* InventoryLarge      => (AtkUnitBase*)Service.Gui.GetAddonByName("InventoryLarge");
-    public static AtkUnitBase* InventoryExpansion  => (AtkUnitBase*)Service.Gui.GetAddonByName("InventoryExpansion");
-    public static AtkUnitBase* SelectString        => (AtkUnitBase*)Service.Gui.GetAddonByName("SelectString");
-    public static AtkUnitBase* InputNumeric        => (AtkUnitBase*)Service.Gui.GetAddonByName("InputNumeric");
-    public static AtkUnitBase* ChatLog             => (AtkUnitBase*)Service.Gui.GetAddonByName("ChatLog");
-    public static AtkUnitBase* SelectYesno         => (AtkUnitBase*)Service.Gui.GetAddonByName("SelectYesno");
-    public static AtkUnitBase* TitleMenu           => (AtkUnitBase*)Service.Gui.GetAddonByName("_TitleMenu");
-    public static AtkUnitBase* CharaSelectListMenu => (AtkUnitBase*)Service.Gui.GetAddonByName("_CharaSelectListMenu");
-    public static AtkUnitBase* CollectablesShop    => (AtkUnitBase*)Service.Gui.GetAddonByName("CollectablesShop");
-
-    public static AtkUnitBase* GrandCompanySupplyReward =>
-        (AtkUnitBase*)Service.Gui.GetAddonByName("GrandCompanySupplyReward");
-    public static AtkUnitBase* GrandCompanySupplyList =>
-        (AtkUnitBase*)Service.Gui.GetAddonByName("GrandCompanySupplyList");
-    public static AtkUnitBase* CharaSelectWorldServer =>
-        (AtkUnitBase*)Service.Gui.GetAddonByName("_CharaSelectWorldServer");
+    public static AtkUnitBase* Inventory                => GetAddonByName("Inventory");
+    public static AtkUnitBase* InventoryLarge           => GetAddonByName("InventoryLarge");
+    public static AtkUnitBase* InventoryExpansion       => GetAddonByName("InventoryExpansion");
+    public static AtkUnitBase* SelectString             => GetAddonByName("SelectString");
+    public static AtkUnitBase* InputNumeric             => GetAddonByName("InputNumeric");
+    public static AtkUnitBase* ChatLog                  => GetAddonByName("ChatLog");
+    public static AtkUnitBase* SelectYesno              => GetAddonByName("SelectYesno");
+    public static AtkUnitBase* TitleMenu                => GetAddonByName("_TitleMenu");
+    public static AtkUnitBase* CharaSelectListMenu      => GetAddonByName("_CharaSelectListMenu");
+    public static AtkUnitBase* CollectablesShop         => GetAddonByName("CollectablesShop");
+    public static AtkUnitBase* GrandCompanySupplyReward => GetAddonByName("GrandCompanySupplyReward");
+    public static AtkUnitBase* GrandCompanySupplyList   => GetAddonByName("GrandCompanySupplyList");
+    public static AtkUnitBase* CharaSelectWorldServer   => GetAddonByName("_CharaSelectWorldServer");
 
     public static AtkUnitBase* ToAtkUnitBase(this nint ptr) => (AtkUnitBase*)ptr;
+
+    public static AtkUnitBase* GetAddonByName(string name) => (AtkUnitBase*)Service.Gui.GetAddonByName(name);
 }
