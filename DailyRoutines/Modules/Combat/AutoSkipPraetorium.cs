e7edd2ed943979fef25d7b46755d1271820f906c
AtmoOmen <atmoomen2004@outlook.com>
2024-06-18 23:25:45 +0800

    Update AutoSkipPraetorium.cs

--- DailyRoutines/Modules/Combat/AutoSkipPraetorium.cs
+++ DailyRoutines/Modules/Combat/AutoSkipPraetorium.cs
@@ -1,6 +1,9 @@
 using System;
 using DailyRoutines.Managers;
 using Dalamud;
+using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 namespace DailyRoutines.Modules;
 
@@ -8,21 +11,29 @@ namespace DailyRoutines.Modules;
 [ModuleDescription("AutoSkipPraetoriumTitle", "AutoSkipPraetoriumDescription", ModuleCategories.战斗)]
 public class AutoSkipPraetorium : DailyModuleBase
 {
-    public bool Valid   => Offset1 != IntPtr.Zero && Offset2 != IntPtr.Zero;
-    public nint Offset1 { get; private set; }
-    public nint Offset2 { get; private set; }
+    public static bool Valid   => Offset1 != nint.Zero && Offset2 != nint.Zero;
+    public static nint Offset1 { get; private set; }
+    public static nint Offset2 { get; private set; }
+
+    private unsafe delegate bool IsCutsceneSeenDelegate(PlayerState* instance, uint a2);
+    [Signature(
+        "E8 ?? ?? ?? ?? 33 C9 0F B6 DB 3C ?? 0F 44 D9 0F B6 D3 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B 5C 24 ?? B8 ?? ?? ?? ?? 48 83 C4 ?? 5F C3 CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC",
+        DetourName = nameof(IsCutsceneSeenDetour))]
+    private static Hook<IsCutsceneSeenDelegate>? IsCutsceneSeenHook;
 
     public override void Init()
     {
+        Service.Hook.InitializeFromAttributes(this);
+        IsCutsceneSeenHook?.Enable();
+
         Offset1 = Service.SigScanner.ScanText
             ("75 33 48 8B 0D ?? ?? ?? ?? BA ?? 00 00 00 48 83 C1 10 E8 ?? ?? ?? ?? 83 78");
-
         Offset2 = Service.SigScanner.ScanText("74 18 8B D7 48 8D 0D");
 
         SetEnabled(Valid);
     }
 
-    public void SetEnabled(bool isEnable)
+    public static void SetEnabled(bool isEnable)
     {
         if (!Valid) return;
 
@@ -32,6 +43,8 @@ public class AutoSkipPraetorium : DailyModuleBase
         SafeMemory.Write(Offset1, value1);
         SafeMemory.Write(Offset2, value2);
     }
+
+    private static unsafe bool IsCutsceneSeenDetour(PlayerState* instance, uint a2) { return true; }
 
     public override void Uninit()
     {
