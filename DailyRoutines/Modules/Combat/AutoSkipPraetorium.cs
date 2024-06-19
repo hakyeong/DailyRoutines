81badd6dc856fc62f1a47f88de19438b2739060c
AtmoOmen <atmoomen2004@outlook.com>
2024-06-19 10:38:30 +0800

    Update AutoSkipPraetorium.cs

--- DailyRoutines/Modules/Combat/AutoSkipPraetorium.cs
+++ DailyRoutines/Modules/Combat/AutoSkipPraetorium.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DailyRoutines.Managers;
 using Dalamud;
 using Dalamud.Hooking;
@@ -6,49 +7,49 @@ using FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 namespace DailyRoutines.Modules;
 
-// 完全来自 Dalamud.SkipCutScene
+// 主要逻辑来自 Dalamud.SkipCutScene
 [ModuleDescription("AutoSkipPraetoriumTitle", "AutoSkipPraetoriumDescription", ModuleCategories.战斗)]
 public class AutoSkipPraetorium : DailyModuleBase
 {
-    public static bool Valid   => Offset1 != nint.Zero && Offset2 != nint.Zero;
-    public static nint Offset1 { get; private set; }
-    public static nint Offset2 { get; private set; }
+    private static readonly nint Offset1 = Service.SigScanner.ScanText
+        ("75 33 48 8B 0D ?? ?? ?? ?? BA ?? 00 00 00 48 83 C1 10 E8 ?? ?? ?? ?? 83 78");
+    private static readonly nint Offset2 = Service.SigScanner.ScanText
+        ("74 18 8B D7 48 8D 0D");
+
+    private static readonly HashSet<uint> Zones = [1043, 1044, 1048];
+    private const short EnableValue = -28528;
+    private const short DisableValue = 13173;
+    private const short DisableValue2 = 6260;
 
     private unsafe delegate bool IsCutsceneSeenDelegate(PlayerState* instance, uint a2);
-    [Signature(
-        "E8 ?? ?? ?? ?? 33 C9 0F B6 DB 3C ?? 0F 44 D9 0F B6 D3 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B 5C 24 ?? B8 ?? ?? ?? ?? 48 83 C4 ?? 5F C3 CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC",
-        DetourName = nameof(IsCutsceneSeenDetour))]
+    [Signature("E8 ?? ?? ?? ?? 33 C9 0F B6 DB 3C ?? 0F 44 D9 0F B6 D3 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B 5C 24 ?? B8 ?? ?? ?? ?? 48 83 C4 ?? 5F C3 CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC", DetourName = nameof(IsCutsceneSeenDetour))]
     private static Hook<IsCutsceneSeenDelegate>? IsCutsceneSeenHook;
 
     public override void Init()
     {
         Service.Hook.InitializeFromAttributes(this);
         IsCutsceneSeenHook?.Enable();
-
-        Offset1 = Service.SigScanner.ScanText
-            ("75 33 48 8B 0D ?? ?? ?? ?? BA ?? 00 00 00 48 83 C1 10 E8 ?? ?? ?? ?? 83 78");
-        Offset2 = Service.SigScanner.ScanText("74 18 8B D7 48 8D 0D");
-
-        SetEnabled(Valid);
+        SetEnabled(IsOffsetsValid());
     }
 
-    public static void SetEnabled(bool isEnable)
+    private static bool IsOffsetsValid() => Offset1 != nint.Zero && Offset2 != nint.Zero;
+
+    private static void SetEnabled(bool isEnabled)
     {
-        if (!Valid) return;
+        if (!IsOffsetsValid()) return;
 
-        var value1 = isEnable ? (short)-28528 : (short)13173;
-        var value2 = isEnable ? (short)-28528 : (short)6260;
-
-        SafeMemory.Write(Offset1, value1);
-        SafeMemory.Write(Offset2, value2);
+        SafeMemory.Write(Offset1, isEnabled ? EnableValue : DisableValue);
+        SafeMemory.Write(Offset2, isEnabled ? EnableValue : DisableValue2);
     }
 
-    private static unsafe bool IsCutsceneSeenDetour(PlayerState* instance, uint a2) { return true; }
+    private static unsafe bool IsCutsceneSeenDetour(PlayerState* instance, uint a2)
+        => Zones.Contains(Service.ClientState.TerritoryType) || IsCutsceneSeenHook.Original(instance, a2);
 
     public override void Uninit()
     {
-        if (Initialized) SetEnabled(false);
+        SetEnabled(false);
 
         base.Uninit();
     }
 }
+
