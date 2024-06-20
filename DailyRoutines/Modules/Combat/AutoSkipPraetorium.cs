1ce9db52b5e6b295c0061bb919be17df7ab229ef
AtmoOmen <atmoomen2004@outlook.com>
2024-06-20 22:07:41 +0800

    To MemoryPatch

--- DailyRoutines/Modules/Combat/AutoSkipPraetorium.cs
+++ DailyRoutines/Modules/Combat/AutoSkipPraetorium.cs
@@ -1,46 +1,23 @@
-using DailyRoutines.Managers;
-using Dalamud;
+using DailyRoutines.Infos;
 
 namespace DailyRoutines.Modules;
 
 [ModuleDescription("AutoSkipPraetoriumTitle", "AutoSkipPraetoriumDescription", ModuleCategories.战斗)]
 public class AutoSkipPraetorium : DailyModuleBase
 {
-    private static nint    Ptr       { get; set; }
-    private static byte[]? OrigBytes { get; set; }
+    private static readonly MemoryPatch IsPlayCutscenePatch =
+        new("0F B6 D3 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B 5C 24 ?? B8 ?? ?? ?? ?? 48 83 C4 ?? 5F C3 CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC", [0x31, 0xD2, 0x90]);
 
     public override void Init()
     {
-        if (Service.SigScanner.TryScanText("0F B6 D3 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B 5C 24 ?? B8 ?? ?? ?? ?? 48 83 C4 ?? 5F C3 CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC", out var ptr))
-            Ptr = ptr;
+        if (!IsPlayCutscenePatch.IsValid) return;
 
-        SetEnabled(true);
-    }
-
-    private static void SetEnabled(bool isEnabled)
-    {
-        if (Ptr == nint.Zero) return;
-
-        if (isEnabled)
-        {
-            if (SafeMemory.ReadBytes(Ptr, 3, out var origBytes))
-                OrigBytes ??= origBytes;
-
-            // movzx, edx, bl -> xor edx, edx, nop
-            SafeMemory.WriteBytes(Ptr, [0x31, 0xD2, 0x90]);
-        }
-        else
-        {
-            if (OrigBytes == null) return;
-
-            SafeMemory.WriteBytes(Ptr, OrigBytes);
-            OrigBytes = null;
-        }
+        IsPlayCutscenePatch.Set(true);
     }
 
     public override void Uninit()
     {
-        SetEnabled(false);
+        IsPlayCutscenePatch.Set(false);
     }
 }
 
