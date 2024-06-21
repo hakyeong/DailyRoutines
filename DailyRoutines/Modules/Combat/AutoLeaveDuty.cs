47141af5aa46c4af4b53a206feb2cfe5907c1d78
AtmoOmen <atmoomen2004@outlook.com>
2024-06-21 15:34:24 +0800

    强制退本

--- DailyRoutines/Modules/Combat/AutoLeaveDuty.cs
+++ DailyRoutines/Modules/Combat/AutoLeaveDuty.cs
@@ -11,20 +11,38 @@ namespace DailyRoutines.Modules;
 [ModuleDescription("AutoLeaveDutyTitle", "AutoLeaveDutyDescription", ModuleCategories.战斗)]
 public class AutoLeaveDuty : DailyModuleBase
 {
-    private static HashSet<uint> BlacklistContents = [];
+    private class Config : ModuleConfiguration
+    {
+        public HashSet<uint> BlacklistContents = [];
+        public bool ForceToLeave;
+    }
+
+    private static Config ModuleConfig = null!;
     private static string ContentSearchInput = string.Empty;
 
     public override void Init()
     {
-        AddConfig(nameof(BlacklistContents), BlacklistContents);
-        BlacklistContents = GetConfig<HashSet<uint>>(nameof(BlacklistContents));
+        ModuleConfig = LoadConfig<Config>() ?? new();
+
+        TaskHelper ??= new();
 
         Service.DutyState.DutyCompleted += OnDutyComplete;
     }
 
     public override void ConfigUI()
     {
-        ImGui.TextColored(ImGuiColors.TankBlue, Service.Lang.GetText("AutoLeaveDuty-Note"));
+        ImGui.AlignTextToFramePadding();
+        ImGui.TextColored(ImGuiColors.DalamudOrange, $"{Service.Lang.GetText("AutoLeaveDuty-ForceToLeave")}:");
+
+        ImGui.SameLine();
+        if (ImGui.Checkbox("###ForceToLeave", ref ModuleConfig.ForceToLeave))
+            SaveConfig(ModuleConfig);
+
+        if (ModuleConfig.ForceToLeave)
+        {
+            ImGui.SameLine();
+            ImGui.TextColored(ImGuiColors.TankBlue, Service.Lang.GetText("AutoLeaveDuty-Note"));
+        }
 
         ImGui.Spacing();
 
@@ -33,26 +51,40 @@ public class AutoLeaveDuty : DailyModuleBase
 
         ImGui.SameLine();
         ImGui.SetNextItemWidth(250f * ImGuiHelpers.GlobalScale);
-        if (ContentSelectCombo(ref BlacklistContents, ref ContentSearchInput))
-            UpdateConfig(nameof(BlacklistContents), BlacklistContents);
+        if (ContentSelectCombo(ref ModuleConfig.BlacklistContents, ref ContentSearchInput))
+            SaveConfig(ModuleConfig);
 
         ImGui.SameLine();
         if (ImGui.Button(Service.Lang.GetText("AutoLeaveDuty-AddCurrentHighEnd")))
         {
-            BlacklistContents.UnionWith(PresetData.HighEndContents.Keys);
-            UpdateConfig(nameof(BlacklistContents), BlacklistContents);
+            ModuleConfig.BlacklistContents.UnionWith(PresetData.HighEndContents.Keys);
+            SaveConfig(ModuleConfig);
         }
     }
 
-    private static void OnDutyComplete(object? sender, ushort zone)
+    private void OnDutyComplete(object? sender, ushort zone)
     {
-        if (BlacklistContents.Contains(zone)) return;
+        if (ModuleConfig.BlacklistContents.Contains(zone)) return;
+        if (!ModuleConfig.ForceToLeave)
+        {
+            TaskHelper.Enqueue(() =>
+            {
+                if (Service.Condition[ConditionFlag.InCombat]) return false;
 
-        Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.LeaveDuty, Service.Condition[ConditionFlag.InCombat] ? 1 : 0);
+                Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.LeaveDuty);
+                return true;
+            });
+        }
+        else
+        {
+            Service.ExecuteCommandManager.ExecuteCommand(ExecuteCommandFlag.LeaveDuty, Service.Condition[ConditionFlag.InCombat] ? 1 : 0);
+        }
     }
 
     public override void Uninit()
     {
         Service.DutyState.DutyCompleted -= OnDutyComplete;
+
+        base.Uninit();
     }
 }
