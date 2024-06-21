0c09fa7080feb8def93bdd88aa1a6b34ccd3c6e4
AtmoOmen <atmoomen2004@outlook.com>
2024-06-21 22:47:40 +0800

    Update FontHelper.cs

--- DailyRoutines/Helpers/FontHelper.cs
+++ DailyRoutines/Helpers/FontHelper.cs
@@ -1,24 +1,92 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 using DailyRoutines.Managers;
 using Dalamud.Interface.GameFonts;
 using Dalamud.Interface.ManagedFontAtlas;
+using Dalamud.Interface.Utility;
 using ImGuiNET;
 
 namespace DailyRoutines.Helpers;
 
 public class FontHelper
 {
-    public static IFontAtlas FontAtlas => fontAtlas.Value;
-    public static IFontHandle Axis96 => axis96.Value;
-    public static IFontHandle Axis12 => axis12.Value;
-    public static IFontHandle Axis14 => axis14.Value;
-    public static IFontHandle Axis18 => axis18.Value;
-    public static IFontHandle Icon => Service.PluginInterface.UiBuilder.IconFontHandle;
+    public static IFontAtlas  FontAtlas => fontAtlas.Value;
+    public static IFontHandle Icon      => Service.PluginInterface.UiBuilder.IconFontHandle;
+
+    public static IFontHandle UIFont { get; set; } = null!;
+    private static float OldFontSize { get; set; }
 
     private static IFontHandle ConstructFontHandle(GameFontFamilyAndSize fontInfo)
         => FontAtlas.NewGameFontHandle(new GameFontStyle(fontInfo));
 
-    public static unsafe ImFontPtr GetFont(float size, float scale = 0)
+    public static void RefreshUIFont()
+    {
+        if (OldFontSize == Service.Config.InterfaceFontSize) return;
+        OldFontSize = Service.Config.InterfaceFontSize;
+
+        string path;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            path = Path.Join("C:\\Windows\\Fonts", "msyh.ttc");
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            path = "/System/Library/Fonts/PingFang.ttc";
+        }
+        else
+        {
+            throw new PlatformNotSupportedException("不支持的操作系统");
+        }
+
+        UIFont = FontAtlas.NewDelegateFontHandle(e =>
+        {
+            e.OnPreBuild(tk => tk.AddFontFromFile(path, new()
+            {
+                SizePt = Service.Config.InterfaceFontSize,
+                PixelSnapH = true,
+                GlyphRanges = BuildRange(null, [ImGui.GetIO().Fonts.GetGlyphRangesChineseFull(), ImGui.GetIO().Fonts.GetGlyphRangesDefault()]),
+            }));
+        });
+    }
+
+    public static unsafe ushort[] BuildRange(IReadOnlyList<ushort>? chars, params nint[] ranges)
+    {
+        var builder = new ImFontGlyphRangesBuilderPtr(ImGuiNative.ImFontGlyphRangesBuilder_ImFontGlyphRangesBuilder());
+        foreach (var range in ranges)
+            builder.AddRanges(range);
+
+        if (chars != null)
+        {
+            for (var i = 0; i < chars.Count; i += 2)
+            {
+                if (chars[i] == 0)
+                    break;
+
+                for (var j = (uint)chars[i]; j <= chars[i + 1]; j++)
+                    builder.AddChar((ushort)j);
+            }
+        }
+
+        for (ushort i = 'A'; i <= 'Z'; i++)
+            builder.AddChar(i);
+        for (ushort i = 'a'; i <= 'z'; i++)
+            builder.AddChar(i);
+
+        builder.AddText("←→↑↓《》■※☀★★☆♥♡ヅツッシ☀☁☂℃℉°♀♂♠♣♦♣♧®©™€$£♯♭♪✓√◎◆◇♦■□〇●△▽▼▲‹›≤≥<«“”─＼～");
+        builder.AddText("Œœ");
+        builder.AddText("ĂăÂâÎîȘșȚț");
+
+        for (var i = 0x2460; i <= 0x24B5; i++)
+            builder.AddChar((char)i);
+
+        builder.AddChar('⓪');
+        return builder.BuildRangesToArray();
+    }
+
+    public static unsafe ImFontPtr GetFontPtr(float size, float scale = 0)
     {
         var style = new GameFontStyle(GameFontStyle.GetRecommendedFamilyAndSize(GameFontFamily.Axis, size));
         var handle = FontAtlas.NewGameFontHandle(style);
@@ -39,6 +107,14 @@ public class FontHelper
         {
             return ImGui.GetFont();
         }
+    }
+
+    public static IFontHandle GetFontHandle(float size, float scale = 0)
+    {
+        var style = new GameFontStyle(GameFontFamily.Axis, size);
+        var handle = FontAtlas.NewGameFontHandle(style);
+        
+        return handle;
     }
 
     #region Lazy
