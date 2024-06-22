8f9bbffbd89c88e77f76fed36faa73e972ca3b64
AtmoOmen <atmoomen2004@outlook.com>
2024-06-22 11:49:12 +0800

    AutoSkipDutyCutscene

--- DailyRoutines/Modules/Combat/AutoSkipDutyCutscene.cs
+++ DailyRoutines/Modules/Combat/AutoSkipDutyCutscene.cs
@@ -2,8 +2,8 @@ using DailyRoutines.Infos;
 
 namespace DailyRoutines.Modules;
 
-[ModuleDescription("AutoSkipPraetoriumTitle", "AutoSkipPraetoriumDescription", ModuleCategories.战斗)]
-public class AutoSkipPraetorium : DailyModuleBase
+[ModuleDescription("AutoSkipDutyCutsceneTitle", "AutoSkipDutyCutsceneDescription", ModuleCategories.战斗)]
+public class AutoSkipDutyCutscene : DailyModuleBase
 {
     private static readonly MemoryPatch IsPlayCutscenePatch =
         new("0F B6 D3 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B 5C 24 ?? B8 ?? ?? ?? ?? 48 83 C4 ?? 5F C3 CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC", [0x31, 0xD2, 0x90]);
@@ -20,4 +20,3 @@ public class AutoSkipPraetorium : DailyModuleBase
         IsPlayCutscenePatch.Set(false);
     }
 }
-
