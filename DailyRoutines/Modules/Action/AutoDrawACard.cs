6f7aa1a92562d767c1067a3a219d0cdf3474eab3
AtmoOmen <atmoomen2004@outlook.com>
2024-07-01 21:16:48 +0800

    Update AutoDrawACard.cs

--- DailyRoutines/Modules/Action/AutoDrawACard.cs
+++ DailyRoutines/Modules/Action/AutoDrawACard.cs
@@ -24,22 +24,27 @@ public class AutoDrawACard : DailyModuleBase
 
     private void OnZoneChanged(ushort zone)
     {
+        TaskHelper.Abort();
         if (!PresetData.Contents.ContainsKey(zone) || Service.ClientState.IsPvP) return;
 
-        TaskHelper.Abort();
         TaskHelper.DelayNext(1000);
         TaskHelper.Enqueue(CheckCurrentJob);
     }
 
-    private static unsafe bool? CheckCurrentJob()
+    private unsafe bool? CheckCurrentJob()
     {
-        if (Flags.BetweenAreas) return false;
-        if (!IsScreenReady()) return false;
+        if (Flags.BetweenAreas || !IsScreenReady()) return false;
 
         var player = Service.ClientState.LocalPlayer;
-        if (player == null || player.ClassJob.Id == 0 || !player.IsTargetable) return false;
+        var job = player?.ClassJob.Id ?? 0;
+        if (player == null || job == 0 || !player.IsTargetable) return false;
 
-        if (player.ClassJob.Id != 33 || player.Level < 30) return true;
+        if (job != 33 || player.Level < 30)
+        {
+            TaskHelper.Abort();
+            return true;
+        }
+        
         if (Flags.OccupiedInEvent) return false;
 
         return ActionManager.Instance()->UseAction(ActionType.Action, 3590);
